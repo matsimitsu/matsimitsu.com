bfcade42a758fa5d79818675cf0b0e80a4df0570
Robert Beekman <robertbeekman@matsimitsu.nl>
2021-05-08 13:52:35 +0200

    Add the final days of the usa 2011 trip

--- src/routes/trips/usa-2011/day-sixteen-seventeen/index.svelte
+++ src/routes/trips/usa-2011/day-sixteen-seventeen/index.svelte
@@ -6,10 +6,10 @@
 		startDate: new Date('2011-07-18'),
 		endDate: new Date('2011-07-19'),
 		image: {
-			src: 'https://cdn.matsimitsu.com/usa-2011/day-fourteen-seventeen/20110715-_DSC0682.jpg',
+			src: 'https://cdn.matsimitsu.com/usa-2011/day-sixteen-seventeen/20110718-_DSC0835-Pano.jpg',
 			width: 2200,
-			height: 1461,
-			alt: 'The Mexican border'
+			height: 1424,
+			alt: 'View of Yosemite National Park'
 		}
 	};
 </script>
@@ -134,3 +134,18 @@
 	height="994"
 	alt="Lake at Yosemite National Park"
 />
+
+<TextContent>
+	<p>
+		After driving through the evening we stopped at <strong>Reno</strong>, the biggest little city
+		in the world.
+	</p>
+	<PhonePic
+		><ResponsiveImage
+			src="https://cdn.matsimitsu.com/usa-2011/day-sixteen-seventeen/20110719-IMG_01031.jpg"
+			width="2200"
+			height="1650"
+			alt="Reno sign"
+		/>
+	</PhonePic>
+</TextContent>
