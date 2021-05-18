aa8b30eb7c0198bcf7a7fb3de7e8528d3fecc13a
Robert Beekman <robertbeekman@matsimitsu.nl>
2021-05-18 20:25:27 +0200

    Fix hover for main nav

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,24 +1,29 @@
 <script>
-  import DarkModeToggle from '$lib/DarkModeToggle.svelte'
+	import NavLink from '$lib/NavLink.svelte';
+	import DarkModeToggle from '$lib/DarkModeToggle.svelte';
 </script>
 
 <nav class="mx-auto px-4 text-lg flex justify-between pt-4 border-b pb-2 mb-4 dark:border-gray-700">
-  <div class="flex">
-    <a href="/" class="hoverable"><h1 class="dark:text-gray-100">Matsimitsu</h1></a>
-    <h3 class="border-l dark:border-gray-600 ml-2 pl-2 text-gray-600 hidden md:block dark:text-gray-400">Robert Beekman</h3>
-  </div>
-  <ul class="flex items-center dark:text-gray-200">
-    <li class="ml-4"><a class="hoverable" sveltekit:prefetch href="/blog">Blog</a></li>
-    <li class="ml-4"><a class="hoverable" sveltekit:prefetch href="/trips">Trips</a></li>
-    <li class="ml-4"><a class="hoverable" sveltekit:prefetch href="/notes">Notes</a></li>
-    <li class="ml-4"><a class="hoverable" sveltekit:prefetch href="/about">About</a></li>
-    <li class="ml-4"><a class="hoverable" sveltekit:prefetch href="/uses">Tech</a></li>
-    <li class="ml-4 hoverable"><DarkModeToggle /></li>
-  </ul>
+	<div class="flex">
+		<a href="/" class="hoverable"><h1 class="dark:text-gray-100">Matsimitsu</h1></a>
+		<h3
+			class="border-l dark:border-gray-600 ml-2 pl-2 text-gray-600 hidden md:block dark:text-gray-400"
+		>
+			Robert Beekman
+		</h3>
+	</div>
+	<ul class="flex items-center dark:text-gray-200">
+		<li class="ml-4"><NavLink href="/blog" title="Blog" /></li>
+		<li class="ml-4"><NavLink href="/trips" title="Trips" /></li>
+		<li class="ml-4"><NavLink href="/notes" title="Notes" /></li>
+		<li class="ml-4"><NavLink href="/about" title="About" /></li>
+		<li class="ml-4"><NavLink href="/uses" title="Tech" /></li>
+		<li class="ml-4 hoverable"><DarkModeToggle /></li>
+	</ul>
 </nav>
 
 <style>
-  .hoverable {
-    @apply hover:opacity-50 opacity-100 duration-200 transition transition-opacity;
-  }
+	.hoverable {
+		@apply hover:opacity-70 opacity-100 duration-200 transition transition-opacity;
+	}
 </style>
---
+++ src/lib/NavLink.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { page } from '$app/stores';
+	export let href;
+	export let title;
+
+	$: active = $page.path.startsWith(href);
+</script>
+
+<a class="hoverable" class:active sveltekit:prefetch {href}>{title}</a>
+
+<style>
+	.hoverable {
+		@apply hover:opacity-100 opacity-70 duration-200 transition transition-opacity;
+	}
+	.active {
+		@apply opacity-100 font-semibold
+	}
+</style>
