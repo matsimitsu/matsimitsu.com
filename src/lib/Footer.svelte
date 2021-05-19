38f6b131502d7233da50868fc712024f39a8aecf
Robert Beekman <robertbeekman@matsimitsu.nl>
2021-05-19 17:21:58 +0200

    Play around with the footer some more

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,53 +1,53 @@
 <footer class="footer-1 bg-gray-100 py-8 sm:py-12 text-gray-600 dark:bg-gray-800 dark:text-gray-400">
-  <div class="container mx-auto px-4">
+  <div class="max-w-6xl mx-auto px-4">
     <div class="sm:flex sm:flex-wrap sm:-mx-4 md:py-4 px-4">
-      <div class="px-4 w-1/4">
+      <div class="px-4 w-1/2 sm:w-1/4">
         <h5 class="text-xl font-bold mb-6">Content</h5>
         <ul class="list-none footer-links">
           <li class="mb-2">
-            <a href="/blog" class="hover:text-blue-500">Blog</a>
+            <a href="/blog" class="hover:text-blue-400 hover:underline">Blog</a>
           </li>
           <li class="mb-2">
-            <a href="/trips" class="hover:text-blue-500">Trips</a>
+            <a href="/trips" class="hover:text-blue-400 hover:underline">Trips</a>
           </li>
           <li class="mb-2">
-            <a href="/notes" class="hover:text-blue-500">Notes</a>
+            <a href="/notes" class="hover:text-blue-400 hover:underline">Notes</a>
           </li>
         </ul>
       </div>
-      <div class="px-4 w-1/4">
+      <div class="px-4 w-1/4 mt-8 sm:mt-0">
         <h5 class="text-xl font-bold mb-6">About</h5>
         <ul class="list-none footer-links">
           <li class="mb-2">
-            <a href="/about" class="hover:text-blue-500">About me</a>
+            <a href="/about" class="hover:text-blue-400 hover:underline">About me</a>
           </li>
           <li class="mb-2">
-            <a href="/uses" class="hover:text-blue-500">Tech</a>
+            <a href="/uses" class="hover:text-blue-400 hover:underline">Tech</a>
           </li>
         </ul>
       </div>
-      <div class="px-4 w-1/4">
+      <div class="px-4 w-1/4 mt-8 sm:mt-0">
         <h5 class="text-xl font-bold mb-6">Feeds</h5>
         <ul class="list-none footer-links">
           <li class="mb-2">
-            <a href="/feeds/blog.xml" class="hover:text-blue-500">Blog</a>
+            <a href="/feeds/blog.xml" class="hover:text-blue-400 hover:underline">Blog</a>
           </li>
           <li class="mb-2">
-            <a href="/feeds/trips.xml" class="hover:text-blue-500">Trips</a>
+            <a href="/feeds/trips.xml" class="hover:text-blue-400 hover:underline">Trips</a>
           </li>
           <li class="mb-2">
-            <a href="/feeds/notes.xml" class="hover:text-blue-500">Notes</a>
+            <a href="/feeds/notes.xml" class="hover:text-blue-400 hover:underline">Notes</a>
           </li>
         </ul>
       </div>
-      <div class="px-4 w-1/4">
+      <div class="px-4 w-1/4 mt-8 sm:mt-0">
         <h5 class="text-xl font-bold mb-6 ">Credits</h5>
 				<ul class="list-none footer-links">
           <li class="mb-2">
-            Icons by <a class="hover:text-blue-500" href="https://fontawesome.com">FontAwesome</a>
+            Icons by <a class="text-blue-400 hover:underline" href="https://fontawesome.com">FontAwesome</a>
           </li>
           <li class="mb-2">
-            Maps from <a class="hover:text-blue-500" href="https://github.com/topojson/world-atlas">Topojson</a>
+            Maps from <a class="text-blue-400 hover:underline" href="https://github.com/topojson/world-atlas">Topojson</a>
           </li>
         </ul>
       </div>
@@ -57,18 +57,18 @@
       <div class="px-4">
 				Made with
 				<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="heart" class="inline text-red-500 mb-1 h-4 w-4" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M462.3 62.6C407.5 15.9 326 24.3 275.7 76.2L256 96.5l-19.7-20.3C186.1 24.3 104.5 15.9 49.7 62.6c-62.8 53.6-66.1 149.8-9.9 207.9l193.5 199.8c12.5 12.9 32.8 12.9 45.3 0l193.5-199.8c56.3-58.1 53-154.3-9.8-207.9z"></path></svg>
-				and <a class="text-blue-400 hover:text-blue-500" href="https://svelte.dev">Svelte</a>
+				and <a class="text-blue-400 hover:underline" href="https://svelte.dev">Svelte</a>
       </div>
       <div class="px-4 text-center mx-auto">
 				&copy {new Date().getFullYear() } Matsimitsu
       </div>
       <div class="flex justify-end px-4">
-				<a href="https://twitter.com/matsimitsu" class="hover:text-blue-500">
+				<a href="https://twitter.com/matsimitsu" class="hover:text-blue-400">
 					<svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="twitter" class="svg-inline--fa fa-twitter h-6 w-6" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"></path></svg>
 				</a>
-				<a href="https://github.com/matsimitsu" class="hover:text-blue-500 ml-4">
+				<a href="https://github.com/matsimitsu" class="hover:text-blue-400 ml-4">
 					<svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="github-alt" class="svg-inline--fa fa-github-alt h-6 w-6" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 480 512"><path fill="currentColor" d="M186.1 328.7c0 20.9-10.9 55.1-36.7 55.1s-36.7-34.2-36.7-55.1 10.9-55.1 36.7-55.1 36.7 34.2 36.7 55.1zM480 278.2c0 31.9-3.2 65.7-17.5 95-37.9 76.6-142.1 74.8-216.7 74.8-75.8 0-186.2 2.7-225.6-74.8-14.6-29-20.2-63.1-20.2-95 0-41.9 13.9-81.5 41.5-113.6-5.2-15.8-7.7-32.4-7.7-48.8 0-21.5 4.9-32.3 14.6-51.8 45.3 0 74.3 9 108.8 36 29-6.9 58.8-10 88.7-10 27 0 54.2 2.9 80.4 9.2 34-26.7 63-35.2 107.8-35.2 9.8 19.5 14.6 30.3 14.6 51.8 0 16.4-2.6 32.7-7.7 48.2 27.5 32.4 39 72.3 39 114.2zm-64.3 50.5c0-43.9-26.7-82.6-73.5-82.6-18.9 0-37 3.4-56 6-14.9 2.3-29.8 3.2-45.1 3.2-15.2 0-30.1-.9-45.1-3.2-18.7-2.6-37-6-56-6-46.8 0-73.5 38.7-73.5 82.6 0 87.8 80.4 101.3 150.4 101.3h48.2c70.3 0 150.6-13.4 150.6-101.3zm-82.6-55.1c-25.8 0-36.7 34.2-36.7 55.1s10.9 55.1 36.7 55.1 36.7-34.2 36.7-55.1-10.9-55.1-36.7-55.1z"></path></svg>				</a>
-				<a href="mailto:hello@matsimitsu.nl" class="hover:text-blue-500 ml-4">
+				<a href="mailto:hello@matsimitsu.nl" class="hover:text-blue-400 ml-4">
 					<svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="envelope" class="svg-inline--fa fa-envelope h-6 w-6" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M464 64H48C21.49 64 0 85.49 0 112v288c0 26.51 21.49 48 48 48h416c26.51 0 48-21.49 48-48V112c0-26.51-21.49-48-48-48zm0 48v40.805c-22.422 18.259-58.168 46.651-134.587 106.49-16.841 13.247-50.201 45.072-73.413 44.701-23.208.375-56.579-31.459-73.413-44.701C106.18 199.465 70.425 171.067 48 152.805V112h416zM48 400V214.398c22.914 18.251 55.409 43.862 104.938 82.646 21.857 17.205 60.134 55.186 103.062 54.955 42.717.231 80.509-37.199 103.053-54.947 49.528-38.783 82.032-64.401 104.947-82.653V400H48z"></path></svg>
 				</a>
       </div>
