0a02da7f863119c40ed119510aa0dac22fc49501
Robert Beekman <robertbeekman@matsimitsu.nl>
2021-06-12 18:20:33 +0200

    Fix image of asia-2014 day-eight and add day-nine

--- src/routes/trips/asia-2014/day-eight.svelte
+++ src/routes/trips/asia-2014/day-eight.svelte
@@ -5,10 +5,10 @@
 		trip: 'asia-2014',
 		startDate: new Date('2014-05-22'),
 		image: {
-			src: 'https://cdn.matsimitsu.com/asia-2014/day-seven/20140521-_DSC0374.jpg',
+			src: 'https://cdn.matsimitsu.com/asia-2014/day-eight/20140522-_DSC0659.jpg',
 			width: 2200,
 			height: 1461,
-			alt: 'Restaurant in Shinjuku'
+			alt: 'Akihabara'
 		}
 	};
 </script>
---
+++ src/routes/trips/asia-2014/day-nine.svelte
@@ -0,0 +1,433 @@
+<script context="module">
+	export const postData = {
+		title: 'Day Nine',
+		subtitle: 'Shinjuku - Ginza - Tsukiji Fish market - Meiji Jingu - Shibuya - Omotesando',
+		trip: 'asia-2014',
+		startDate: new Date('2014-05-23'),
+		image: {
+			src: 'https://cdn.matsimitsu.com/asia-2014/day-nine/20140523-_DSC0692.jpg',
+			width: 2200,
+			height: 1461,
+			alt: 'Shinjuku station'
+		}
+	};
+</script>
+
+<script>
+	import TextContent from '$lib/TextContent.svelte';
+	import ResponsiveImage from '$lib/ResponsiveImage.svelte';
+	import Panels from '$lib/Panels.svelte';
+	import TripHeader from '$lib/TripHeader.svelte';
+	import Video from '$lib/Video.svelte';
+</script>
+
+<TripHeader {...postData} />
+
+<TextContent>
+	<h2>Shinjuku</h2>
+	<p>Today we started out in Shinjuku where we got our morning coffee.</p>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2014/day-nine/20140523-_DSC0688.jpg"
+	width="2200"
+	height="1461"
+	alt=""
+/>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2014/day-nine/20140523-_DSC0690.jpg"
+		width="2200"
+		height="1461"
+		alt=""
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2014/day-nine/20140523-_DSC0692.jpg"
+		width="2200"
+		height="1461"
+		alt=""
+	/>
+</Panels>
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2014/day-nine/20140523-_DSC0698.jpg"
+	width="2200"
+	height="1461"
+	alt=""
+/>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2014/day-nine/20140523-_DSC0695.jpg"
+		width="1461"
+		height="2200"
+		alt=""
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2014/day-nine/20140523-_DSC0702.jpg"
+		width="2200"
+		height="1461"
+		alt=""
+	/>
+</Panels>
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2014/day-nine/20140523-_DSC0703.jpg"
+	width="2200"
+	height="1461"
+	alt=""
+/>
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2014/day-nine/20140523-_DSC0706.jpg"
+		width="2200"
+		height="1628"
+		alt=""
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2014/day-nine/20140523-_DSC0707.jpg"
+		width="2200"
+		height="1461"
+		alt=""
+	/>
+</Panels>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2014/day-nine/20140523-_DSC0709.jpg"
+	width="2200"
+	height="1461"
+	alt=""
+/>
+
+<TextContent
+	><p>
+		From there we took the train to the Imperial Palace
+	</p></TextContent
+>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2014/day-nine/20140523-_DSC0715.jpg"
+	width="2200"
+	height="1461"
+	alt=""
+/>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2014/day-nine/20140523-_DSC0718.jpg"
+		width="2200"
+		height="1461"
+		alt=""
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2014/day-nine/20140523-_DSC0722.jpg"
+		width="2200"
+		height="1461"
+		alt=""
+	/>
+</Panels>
+<TextContent><p>Unfortunately it was closed (on a friday!).
+  So back to the train it was.</p></TextContent>
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2014/day-nine/20140523-_DSC0730.jpg"
+	width="2200"
+	height="1461"
+	alt=""
+/>
+
+<TextContent>
+	<h2>Ginza</h2>
+	<p>
+		This time we emerged from the train in Ginza, a shopping district that reminds of 5th avenue in
+		New York.
+	</p>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2014/day-nine/20140523-_DSC0734.jpg"
+	width="2200"
+	height="1461"
+	alt=""
+/>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2014/day-nine/20140523-_DSC0741.jpg"
+	width="2200"
+	height="1461"
+	alt=""
+/>
+<TextContent>
+  <h2>Hamarikyu Gardens</h2>
+	<p>
+		On the south side of Ginza is a park named <strong>Hamarikyu Gardens</strong>, where we ate our convenience-store bought lunch in a nice quiet environment.
+	</p>
+</TextContent>
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2014/day-nine/20140523-_DSC0743.jpg"
+		width="2200"
+		height="1461"
+		alt=""
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2014/day-nine/20140523-_DSC0748.jpg"
+		width="2200"
+		height="1461"
+		alt=""
+	/>
+</Panels>
+
+<TextContent>
+	<h2>Tsukiji Fish market</h2>
+
+	<p>After a nice lunch in a park, we headed for the fish market.</p>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2014/day-nine/20140523-_DSC0751.jpg"
+	width="2200"
+	height="1461"
+	alt=""
+/>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2014/day-nine/20140523-_DSC0753.jpg"
+		width="2200"
+		height="1461"
+		alt=""
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2014/day-nine/20140523-_DSC0755.jpg"
+		width="2200"
+		height="1461"
+		alt=""
+	/>
+</Panels>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2014/day-nine/20140523-_DSC0756.jpg"
+		width="2200"
+		height="1461"
+		alt=""
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2014/day-nine/20140523-_DSC0757.jpg"
+		width="2200"
+		height="1461"
+		alt=""
+	/>
+</Panels>
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2014/day-nine/20140523-_DSC0759.jpg"
+	width="2200"
+	height="1461"
+	alt=""
+/>
+
+<TextContent>
+	<h2>Meiji Jingu</h2>
+	<p>
+		Actually we were just heading to a subway station, the fish market happened to be there :) The
+		subway took us to Yoyogi, where we visited Meiji Jingu, a shrine.
+	</p>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2014/day-nine/20140523-_DSC0761.jpg"
+	width="2200"
+	height="1461"
+	alt=""
+/>
+<ResponsiveImage
+	full
+	src="https://cdn.matsimitsu.com/asia-2014/day-nine/20140523-_DSC0767.jpg"
+	width="2200"
+	height="1461"
+	alt=""
+/>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2014/day-nine/20140523-_DSC0771.jpg"
+		width="2200"
+		height="1461"
+		alt=""
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2014/day-nine/20140523-_DSC0774.jpg"
+		width="2200"
+		height="1461"
+		alt=""
+	/>
+</Panels>
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2014/day-nine/20140523-_DSC0777-2.jpg"
+	width="2200"
+	height="1461"
+	alt=""
+/>
+<TextContent>
+  <p>Meiji Jingu is probably one of the most popular shrine/parks in Tokyo and it's pretty much always busy here.</p>
+</TextContent>
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2014/day-nine/20140523-_DSC0775.jpg"
+		width="2200"
+		height="1461"
+		alt=""
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2014/day-nine/20140523-_DSC0783.jpg"
+		width="2200"
+		height="1282"
+		alt=""
+	/>
+</Panels>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2014/day-nine/20140523-_DSC0780.jpg"
+	width="2200"
+	height="1461"
+	alt=""
+/>
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2014/day-nine/20140523-_DSC0785.jpg"
+	width="2200"
+	height="1461"
+	alt=""
+/>
+<TextContent>
+  <p>Plenty of people were enjoying the weather and admiring the buildings, surrounded by lush greenery and a skyscraper or two.</p>
+</TextContent>
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2014/day-nine/20140523-_DSC0792.jpg"
+	width="2200"
+	height="1461"
+	alt=""
+/>
+<TextContent>
+  <p>The barrels below are used to store Sake, a Japanese liquor also known as rice wine.</p>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2014/day-nine/20140523-_DSC0800.jpg"
+	width="2200"
+	height="1461"
+	alt=""
+/>
+<TextContent>
+	<p>More trains, this time to Shibuya for a picture of the famous crossing.</p>
+</TextContent>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2014/day-nine/20140523-_DSC0805.jpg"
+		width="2200"
+		height="1461"
+		alt=""
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2014/day-nine/20140523-_DSC0808.jpg"
+		width="2200"
+		height="1461"
+		alt=""
+	/>
+</Panels>
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2014/day-nine/20140523-_DSC0811.jpg"
+	width="2200"
+	height="1461"
+	alt=""
+/>
+<TextContent
+	><h2>Shibuya</h2>
+	<p>
+		This is the famous crossing. All sides get the green light at the same time and the crossing
+		fills with people.
+	</p></TextContent
+>
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2014/day-nine/20140523-_DSC0823.jpg"
+	width="2200"
+	height="1461"
+	alt=""
+/>
+<ResponsiveImage
+	full
+	src="https://cdn.matsimitsu.com/asia-2014/day-nine/20140523-_DSC0835.jpg"
+	width="2200"
+	height="1461"
+	alt=""
+/>
+
+<TextContent>
+	<p>The video below was shot on an ancient phone, sorry!</p>
+</TextContent>
+<Video src="https://cdn.matsimitsu.com/asia-2014/day-nine/DSC_0826" title="Shibuya Crossing" />
+
+<TextContent>
+	<h2>Omotesando</h2>
+	<p>
+		After "the freak show" as a local photographer called it, we headed for Omotesando. This is a
+		district where top brands use the weirdest architecture to get customers.
+	</p>
+</TextContent>
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2014/day-nine/20140523-_DSC0839.jpg"
+	width="2200"
+	height="1461"
+	alt=""
+/>
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2014/day-nine/20140523-_DSC0840.jpg"
+		width="2200"
+		height="1615"
+		alt=""
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2014/day-nine/20140523-_DSC0842.jpg"
+		width="2200"
+		height="1461"
+		alt=""
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2014/day-nine/20140523-_DSC0847.jpg"
+		width="1461"
+		height="2200"
+		alt=""
+	/>
+</Panels>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2014/day-nine/20140523-_DSC0845.jpg"
+	width="2200"
+	height="1461"
+	alt=""
+/>
+<TextContent>
+	<p>
+		With our feet becoming heavier by the minute we called it a day and headed back to the hotel on the metro.
+	</p>
+</TextContent>
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2014/day-nine/20140523-_DSC0851.jpg"
+		width="2200"
+		height="1461"
+		alt=""
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2014/day-nine/20140523-_DSC0857.jpg"
+		width="2200"
+		height="1461"
+		alt=""
+	/>
+</Panels>
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2014/day-nine/20140523-_DSC0855.jpg"
+	width="2200"
+	height="1461"
+	alt=""
+/>
