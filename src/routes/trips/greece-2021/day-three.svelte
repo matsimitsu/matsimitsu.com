d5245bf325fb0ae07bc99b92a1f1429a299287a4
Robert Beekman <robertbeekman@matsimitsu.nl>
2021-07-06 22:49:18 +0200

    Add day three of Greece 2021 trip

---
+++ src/routes/trips/greece-2021/day-three.svelte
@@ -0,0 +1,253 @@
+<script context="module">
+	export const postData = {
+		title: 'Day Three',
+		subtitle: 'Walking around Plaka, Psyri and Lycabettus Hill.',
+		trip: 'greece-2021',
+		startDate: new Date('2021-07-06'),
+		endDate: new Date('2021-07-06'),
+		image: {
+			src: 'https://cdn.matsimitsu.com/greece-2021/day-three/20210706-DSC01972-2.jpg',
+			width: 2200,
+			height: 1467,
+			alt: 'Acropolis by night'
+		},
+		locations: []
+	};
+</script>
+
+<script>
+	import TextContent from '$lib/TextContent.svelte';
+	import ResponsiveImage from '$lib/ResponsiveImage.svelte';
+	import Panels from '$lib/Panels.svelte';
+	import TripHeader from '$lib/TripHeader.svelte';
+</script>
+
+<TripHeader {...postData} />
+
+<TextContent>
+	<h2>Plaka</h2>
+	<p>
+		After getting home around midnight last night I took it a bit easier today and just wandered
+		around the city center, eating good food and drinking nice drinks.
+	</p>
+</TextContent>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/greece-2021/day-three/20210706-DSC01921.jpg"
+		width="2200"
+		height="1615"
+		alt="Athens phone boxes"
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/greece-2021/day-three/20210706-DSC01922.jpg"
+		width="1467"
+		height="2200"
+		alt="Athens store"
+	/>
+</Panels>
+
+<ResponsiveImage
+	full
+	src="https://cdn.matsimitsu.com/greece-2021/day-three/20210706-DSC01927.jpg"
+	width="2200"
+	height="1467"
+	alt="Church in Athens"
+/>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/greece-2021/day-three/20210706-DSC01930.jpg"
+		width="1397"
+		height="2200"
+		alt="Inside of church in Athens"
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/greece-2021/day-three/20210706-DSC01931.jpg"
+		width="1467"
+		height="2200"
+		alt="Another church in Athens"
+	/>
+</Panels>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/greece-2021/day-three/20210706-DSC01933.jpg"
+	width="2200"
+	height="1467"
+	alt="Church in Athens"
+/>
+
+<TextContent>
+	<p>
+		While I was taking photos of the church above, these people were riding by. They stopped at the
+		statue where the driver explained a few things and then moved on. A pretty neat way to view the
+		city,
+	</p>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/greece-2021/day-three/20210706-DSC01934.jpg"
+	width="2200"
+	height="1623"
+	alt="Cycle tour"
+/>
+
+<TextContent>
+	<h2>Psyri</h2>
+	<p>
+		Where Plaka is nice and neat, Psyri is a bit more rough around the edges. It used to be a pretty
+		shabby neighbourhood, but since the Olympic games in Athens it has been gentrified... mostly.
+	</p>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/greece-2021/day-three/20210706-DSC01936.jpg"
+	width="2200"
+	height="2131"
+	alt="Meat market in Athens"
+/>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/greece-2021/day-three/20210706-DSC01939.jpg"
+		width="1736"
+		height="2200"
+		alt="Athens graffiti"
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/greece-2021/day-three/20210706-DSC01937.jpg"
+		width="2200"
+		height="1467"
+		alt="Athens graffiti"
+	/>
+</Panels>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/greece-2021/day-three/20210706-DSC01938.jpg"
+		width="2200"
+		height="1467"
+		alt="Athens graffiti"
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/greece-2021/day-three/20210706-DSC01940.jpg"
+		width="1611"
+		height="2200"
+		alt="Athens graffiti"
+	/>
+</Panels>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/greece-2021/day-three/20210706-DSC01944.jpg"
+	width="2200"
+	height="1467"
+	alt="Psyri neighbourhood"
+/>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/greece-2021/day-three/20210706-DSC01946.jpg"
+		width="1467"
+		height="2200"
+		alt="Athens graffiti"
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/greece-2021/day-three/20210706-DSC01947.jpg"
+		width="1467"
+		height="2200"
+		alt="Athens graffiti"
+	/>
+</Panels>
+
+<TextContent>
+	<h2>Greek Parliament</h2>
+	<p>
+		On my way back to the apartment for a quick afternoon refresh (the sun was out in force and it
+		was 35 degrees Celcius) I came across the Greek Parliament where guards stood besides the tomb
+		of the unknown soldier.
+	</p>
+</TextContent>
+
+<ResponsiveImage
+	full
+	src="https://cdn.matsimitsu.com/greece-2021/day-three/20210706-DSC01954.jpg"
+	width="2200"
+	height="1467"
+	alt="Greek parliament"
+/>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/greece-2021/day-three/20210706-DSC01958.jpg"
+	width="2200"
+	height="1259"
+	alt="Woman waiting for the tram"
+/>
+
+<TextContent>
+	<h2>Athens national garden</h2>
+	<p>
+		I have to cross the National garden to get to my apartment, a nice place to buy a cold drink and
+		enjoy the shade before heading on.
+	</p>
+</TextContent>
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/greece-2021/day-three/20210706-DSC01961.jpg"
+	width="2200"
+	height="1467"
+	alt="Athens national garden"
+/>
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/greece-2021/day-three/20210706-DSC01967.jpg"
+		width="2200"
+		height="1467"
+		alt="Athens national garden"
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/greece-2021/day-three/20210706-DSC01968.jpg"
+		width="2200"
+		height="1467"
+		alt="Athens national garden"
+	/>
+</Panels>
+
+<TextContent>
+	<h2>Lycabettus Hill</h2>
+	<p>
+		After a refresh and a delicious Gyros dinner and a few beers I walked a couple of kilometers to
+		Lycabettus Hill, just after sunset. It gives a great view of Athens by dawn.
+	</p>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/greece-2021/day-three/20210706-DSC01985.jpg"
+	width="2200"
+	height="1467"
+	alt="Athens by night"
+/>
+
+<TextContent>
+	<p>On top of the hill (that takes about 10-20 minutes to climb) is a church and a restaurant.</p>
+</TextContent>
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/greece-2021/day-three/20210706-DSC01995.jpg"
+		width="2200"
+		height="1467"
+		alt="Athens by night"
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/greece-2021/day-three/20210706-DSC01988.jpg"
+		width="1095"
+		height="2200"
+		alt="Church cross"
+	/>
+</Panels>
+
+<ResponsiveImage
+	full
+	src="https://cdn.matsimitsu.com/greece-2021/day-three/20210706-DSC01972-2.jpg"
+	width="2200"
+	height="1467"
+	alt="Acropolis by night"
+/>
