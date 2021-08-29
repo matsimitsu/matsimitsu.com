c0e3a84aa10dc26242bef0cde41d22f091e760d5
Robert Beekman <robert@matsimitsu.nl>
2021-08-29 11:07:57 +0200

    Convert trip to a multi-day trip

---
+++ src/routes/trips/poland-2021/day-five.svelte
@@ -0,0 +1,305 @@
+<script context="module">
+	export const postData = {
+		title: 'Day five-six',
+		subtitle: 'Legendia in Katowice',
+		trip: 'poland-2021',
+		startDate: new Date('2021-08-27'),
+		endDate: new Date('2021-08-28'),
+		image: {
+			src: 'https://cdn.matsimitsu.com/poland-2021/20210827-082407512.jpg',
+			width: 2200,
+			height: 1650,
+			alt: 'Ferris wheel Legendia'
+		},
+		locations: [{ name: 'Katowice', long: 18.9739533, lat: 50.2718201 }]
+	};
+</script>
+
+<script>
+	import TextContent from '$lib/TextContent.svelte';
+	import ResponsiveImage from '$lib/ResponsiveImage.svelte';
+	import Panels from '$lib/Panels.svelte';
+	import Video from '$lib/Video.svelte';
+	import TripHeader from '$lib/TripHeader.svelte';
+</script>
+
+<TripHeader {...postData} />
+
+<TextContent>
+	<p>
+		Legendia is a small-ish themepark in/near Katowice and in recent years there have been
+		investments in the park, resulting in a few new rides.
+	</p>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/poland-2021/20210827-081839986.jpg"
+	width="2200"
+	height="1650"
+	alt="Legendia"
+/>
+
+<TextContent>
+	<p>Arriving at 10:10, we were pretty much the only people in the entire park.</p>
+</TextContent>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/poland-2021/20210827-082002480.jpg"
+		width="2200"
+		height="1650"
+		alt="empty"
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/poland-2021/20210827-082244078.jpg"
+		width="2200"
+		height="1650"
+		alt="park"
+	/>
+</Panels>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/poland-2021/20210827-082407512.jpg"
+	width="2200"
+	height="1650"
+	alt="Ferris wheel Legendia"
+/>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/poland-2021/20210827-085310561.jpg"
+		width="2200"
+		height="1650"
+		alt="Roller coaster"
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/poland-2021/20210827-092914704.jpg"
+		width="1650"
+		height="2200"
+		alt="Ferris wheel"
+	/>
+</Panels>
+<TextContent>
+	<p>
+		After a lap through the park we rode the main attraction, <strong>Lech Coaster</strong>, a new
+		Vekoma model.
+	</p>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/poland-2021/20210827-100630252.jpg"
+	width="2200"
+	height="1650"
+	alt="Lech coaster"
+/>
+
+<TextContent>
+	<p>
+		The queue time everywhere was 0 minutes and we could even swap between the back seats and front
+		seats without having to go through the queue again, pretty neat!
+	</p>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/poland-2021/20210827-104744849.jpg"
+	width="2200"
+	height="1650"
+	alt="Lech coaster"
+/>
+
+<TextContent>
+	<p>
+		There was also this russian-made coaster that required at least 3 people per car, otherwise it
+		wouldn't make it to the lift-hill. Luckily for us one of the operators was so kind to accompany
+		us, so we could ride the coaster.
+	</p>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/poland-2021/20210827-095537605.jpg"
+	width="2200"
+	height="1650"
+	alt="Coaster"
+/>
+
+<TextContent>
+	<p>
+		Most of the other atractions were a mix of super-old rides such as this one, from the 1950's and
+		newer, well-themed rides.
+	</p>
+</TextContent>
+
+<Video src="https://cdn.matsimitsu.com/poland-2021/PXL_20210827_082617184" title="Flying things" />
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/poland-2021/20210827-102909789.jpg"
+		width="1650"
+		height="2200"
+		alt="hi"
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/poland-2021/20210827-115244438.jpg"
+		width="2200"
+		height="1650"
+		alt="There"
+	/>
+</Panels>
+
+<TextContent>
+	<p>
+		All the rides to make it a great park are there, you have your log-flume, river rapids, a nice
+		coaster and lots of smaller rides for kids. I'm curious to see how this park will develop over
+		the years.
+	</p>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/poland-2021/20210827-105003695.jpg"
+	width="2200"
+	height="1650"
+	alt="Log flume"
+/>
+
+<TextContent>
+	<h2>Katowice</h2>
+	<p>
+		After spending a couple of hours (and many rides on Lech coaster) in the park, we headed for the
+		exit.
+	</p>
+	<p>
+		There was a cable-car running through the park, but it actually starts outside and goes along
+		the huge park surrounding Legendia.
+	</p>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/poland-2021/20210827-123105010.jpg"
+	width="2200"
+	height="1650"
+	alt="Cable car"
+/>
+
+<TextContent>
+	<p>It's weird having this brand-new cable car going through a park that has seen better days.</p>
+</TextContent>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/poland-2021/20210827-123430694.jpg"
+		width="2200"
+		height="1650"
+		alt="better days"
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/poland-2021/20210827-123700573.jpg"
+		width="2200"
+		height="1650"
+		alt="better days"
+	/>
+</Panels>
+
+<TextContent>
+	<p>Though, other parts have been renovated and are quite nice.</p>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/poland-2021/20210827-125412452.jpg"
+	width="2200"
+	height="1650"
+	alt="park"
+/>
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/poland-2021/20210827-130719698.jpg"
+		width="1650"
+		height="2200"
+		alt="Japanese garden"
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/poland-2021/20210827-131804118.jpg"
+		width="2200"
+		height="1650"
+		alt="Japanese gardens"
+	/>
+</Panels>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/poland-2021/20210827-132407563.jpg"
+		width="2200"
+		height="1650"
+		alt="zoo"
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/poland-2021/20210827-132442456.jpg"
+		width="2200"
+		height="1650"
+		alt="zoo"
+	/>
+</Panels>
+
+<TextContent>
+	<p>From the park we headed back to the hotel along a... well lets say lets touristy area.</p>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/poland-2021/20210827-133930865.jpg"
+	width="2200"
+	height="1650"
+	alt="Back alleys"
+/>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/poland-2021/20210827-134335591.jpg"
+		width="2200"
+		height="1650"
+		alt="back alleys"
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/poland-2021/20210827-135501876.jpg"
+		width="1650"
+		height="2200"
+		alt="old streets"
+	/>
+</Panels>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/poland-2021/20210827-160126112.jpg"
+		width="2200"
+		height="969"
+		alt="Indian food"
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/poland-2021/20210827-162145594.jpg"
+		width="2200"
+		height="770"
+		alt="Indian food"
+	/>
+</Panels>
+
+<TextContent>
+	<h2>Day six: Katowice, Poland</h2>
+	<p>
+		The alarm went off at 03:40 this morning, and after just barely two hours of sleep we pulled
+		ourselves into a taxi to the Katowice airport.
+	</p>
+	<p>Two uneventful flights later we were back in Amsterdam, where I took the train home.</p>
+</TextContent>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/poland-2021/20210828-034559447.jpg"
+		width="2200"
+		height="1650"
+		alt="Katowice airport"
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/poland-2021/20210828-040522856.jpg"
+		width="2200"
+		height="1650"
+		alt="Lufthansa Embraer 190"
+	/>
+</Panels>
---
+++ src/routes/trips/poland-2021/day-one.svelte
@@ -0,0 +1,379 @@
+<script context="module">
+	export const postData = {
+		title: 'Day one-three',
+		subtitle: 'Visiting Energylandia in Poland',
+		trip: 'poland-2021',
+		startDate: new Date('2021-08-23'),
+		endDate: new Date('2021-08-25'),
+		image: {
+			src: 'https://cdn.matsimitsu.com/poland-2021/20210824-085716691.jpg',
+			width: 2200,
+			height: 1650,
+			alt: 'Zadra'
+		},
+		locations: [
+			{ name: 'Energylandia', long: 19.4084671, lat: 49.9974214 },
+			{ name: 'Auschwitz-Birkenau', long: 19.1997942, lat: 50.027414, textPosition: 'left' },
+			{ name: 'Legendia', long: 18.9739533, lat: 50.2718201, textPosition: 'left' },
+			{ name: 'Katowice', long: 18.9739533, lat: 50.2718201 }
+		]
+	};
+</script>
+
+<script>
+	import TextContent from '$lib/TextContent.svelte';
+	import ResponsiveImage from '$lib/ResponsiveImage.svelte';
+	import Panels from '$lib/Panels.svelte';
+	import Video from '$lib/Video.svelte';
+	import Location from '$lib/Location.svelte';
+	import TripHeader from '$lib/TripHeader.svelte';
+	import PhonePic from '$lib/PhonePic.svelte';
+</script>
+
+<TripHeader {...postData} />
+
+<Location
+	name="Trip to Poland"
+	height={480}
+	zoom={3500}
+	focus={true}
+	center={[19, 52]}
+	markers={postData.locations}
+>
+	<p>
+		We arrived at Krakow Airport (KRK) in the evening and made our way to the arrivals hall where we
+		ordered an Uber to our hotel in Zator.
+	</p>
+	<p>
+		Zator is a small town near <strong>Energylandia</strong>, a relatively new (six years old)
+		themepark built in the south of Poland with as of writing no less than <strong>17!</strong> rollercoasters
+		and over a hundred attractions in total (not including food, as that would bring it to 200).
+	</p>
+	<p>The park is subsidised by the European union as are other projects in the south of Poland.</p>
+	<p>
+		In the past few years they've been migrating from a generic themepark to more themed areas like
+		in Disneyland or Universal Studios and the results are amazing.
+	</p>
+</Location>
+
+<TextContent>
+	<h2>Day one, Schiphol, Netherlands</h2>
+	<p>
+		Flying with our favorite airline, Lufthansa, we were stuffed into a tiny CRJ-900 for our flight
+		to Munich, where we would transfer into a bigger A320 to Krakow.
+	</p>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/poland-2021/20210823-113618173.jpg"
+	width="2200"
+	height="1650"
+	alt="lufthansa"
+/>
+
+<Video
+	src="https://cdn.matsimitsu.com/poland-2021/PXL_20210823_124159455"
+	title="take-off from Schiphol"
+/>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/poland-2021/20210823-125226251.jpg"
+		width="2200"
+		height="1650"
+		alt="Amsterdam from above"
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/poland-2021/20210823-144143046.jpg"
+		width="2200"
+		height="1650"
+		alt="A320"
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/poland-2021/20210823-160546734.jpg"
+		width="2200"
+		height="1650"
+		alt="Onboard"
+	/>
+</Panels>
+<TextContent>
+	<p>
+		Thanks to the magic of the Internet we got an Uber to our hotel, which took a nice relaxing
+		ferry along the way. (This did not amuse our driver).
+	</p>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/poland-2021/20210823-175019294.jpg"
+	width="2200"
+	height="1650"
+	alt="Waiting for the ferry"
+/>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/poland-2021/20210823-182513543.jpg"
+		width="2200"
+		height="1650"
+		alt="Beer"
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/poland-2021/20210823-183410447.jpg"
+		width="2200"
+		height="1650"
+		alt="dumplings"
+	/>
+</Panels>
+
+<TextContent>
+	<p>
+		We ended our first night with Beer, Dumplings an pizza and a scary 3Km walk down a creepy road
+		to the hotel in the middle-of-nowhere.
+	</p>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/poland-2021/20210823-194947722.jpg"
+	width="2200"
+	height="1650"
+	alt="creepy road"
+/>
+
+<TextContent>
+	<h2>Day two: Energylandia, Poland</h2>
+	<p>
+		We were in doubt wether to go to the park today or go to another destination first, because of
+		the predicted rain. It was predicted to rain so bad that we each got a text alert on our phone
+		from the government about the rainfall that would fall this day.
+	</p>
+	<p>
+		We decided to go anyway, since we knew for sure that tomorrow would be sunny so we could catch
+		up on any rides we missed today.
+	</p>
+	<p>
+		The first coaster of the day was an Intamin Hypercoaster called Hyperion, which takes you to a
+		height of 77 meters and drops you to the ground at 142 kilometer an hour which exposes you to
+		forces of up to 4.8G.
+	</p>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/poland-2021/20210824-084337796.jpg"
+	width="2200"
+	height="1650"
+	alt="Hyperion coaster at Energylandia"
+/>
+<TextContent>
+	<p>
+		After this excellent wake-up ride we headed to the "wooden" coaster, built by Rocky Mountain
+		Construction (RMC), who are famous for converting old wooden coasters to amazing new rides.
+	</p>
+	<p>This one was built from scratch and is one of the best coasters there is.</p>
+</TextContent>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/poland-2021/20210824-085716691.jpg"
+		width="2200"
+		height="1650"
+		alt="Zadra"
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/poland-2021/20210824-085944791.jpg"
+		width="1650"
+		height="2200"
+		alt="Zadra"
+	/>
+</Panels>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/poland-2021/20210824-085729455.jpg"
+		width="2200"
+		height="1650"
+		alt="not harry potter"
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/poland-2021/20210824-085803420.jpg"
+		width="2200"
+		height="1650"
+		alt="Not Harry Potter"
+	/>
+</Panels>
+
+<PhonePic>
+	<Video src="https://cdn.matsimitsu.com/poland-2021/zadra-trimmed" title="Zadra RMC coaster" />
+</PhonePic>
+
+<TextContent>
+	<p>
+		The latest themed area, completed just a couple of weeks ago is <strong>Aqualantis</strong>,
+		where the <strong>Abyssus</strong> double-launched coaster lives.
+	</p>
+	<p>
+		This one is built by <strong>Vekoma</strong> a Dutch company known for their not-so-great coasters
+		in the past, but they've improved a ton in the past few years.
+	</p>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/poland-2021/20210824-101628936.jpg"
+	width="2200"
+	height="1650"
+	alt="Abyssus"
+/>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/poland-2021/20210824-101733543.jpg"
+	width="2200"
+	height="1650"
+	alt="Abyssus"
+/>
+
+<TextContent>
+	<p>
+		The Aqualantis area is home to a few other rides and restaurants. Among these is a Vekoma
+		Boomerang coaster that, (we think) they got for free with Abyssus..
+	</p>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/poland-2021/20210824-105444423.jpg"
+	width="2200"
+	height="1650"
+	alt="boooomerang"
+/>
+
+<TextContent>
+	<p>
+		Interestingly another Vekoma coaster in this park is called Formula 1 and is a prototype for the
+		Abyssus coaster.
+	</p>
+</TextContent>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/poland-2021/20210824-113428703.jpg"
+		width="2200"
+		height="1650"
+		alt="Formula 1"
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/poland-2021/20210824-113349698.jpg"
+		width="1650"
+		height="2200"
+		alt="Formula 1"
+	/>
+</Panels>
+
+<TextContent>
+	<p>
+		With the famous 4 out of the way we got to explore the rest of the park, including many other
+		coasters, rides and a stunt-show
+	</p>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/poland-2021/20210824-122624958.jpg"
+	width="2200"
+	height="1650"
+	alt="Suspended family coaster"
+/>
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/poland-2021/20210824-123541453.jpg"
+	width="2200"
+	height="1650"
+	alt="Stunt show"
+/>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/poland-2021/20210824-131937814.jpg"
+	width="2200"
+	height="1650"
+	alt="SLC"
+/>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/poland-2021/20210824-150032548.jpg"
+		width="2200"
+		height="1650"
+		alt="Energy coaster"
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/poland-2021/20210824-132150705.jpg"
+		width="2200"
+		height="1650"
+		alt="Boomerang"
+	/>
+</Panels>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/poland-2021/20210824-135255388.jpg"
+	width="2200"
+	height="1650"
+	alt="Suspended family coaster Energylandia"
+/>
+
+<TextContent>
+	<p>
+		In-all the first day was a great success, the queue times for pretty much every ride was 0
+		minutes. I think the longest we waited was 10 minutes for the Formula 1 coasters. The rides such
+		as Hyperion and Zadra, we got ot re-ride instantly a couple of times, thanks to short-cuts in
+		the queue.
+	</p>
+</TextContent>
+
+<TextContent>
+	<h2>Day three: Energylandia, Poland</h2>
+	<p>Our second day in Energylandia, this time with beautiful 20 degrees weather and sunshine.</p>
+	<p>
+		Aparently the rest of Poland also got this message and the 0-minute queue times from yesterday
+		were no more. The shortest we waited today was 30 minutes and the longest 80 minutes....
+	</p>
+	<p>
+		We were so glad we were in this park Yesterday to do most of the rides, as with the amount of
+		people in the park today, we just managed to do 6 rides.
+	</p>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/poland-2021/20210825-082639231.jpg"
+	width="2200"
+	height="1650"
+	alt="Hyperion"
+/>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/poland-2021/20210825-133533459.jpg"
+	width="2200"
+	height="1650"
+	alt="Speed"
+/>
+
+<Video src="https://cdn.matsimitsu.com/poland-2021/splash-trimmed" title="Speed water coaster" />
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/poland-2021/20210825-154241959.jpg"
+	width="2200"
+	height="1650"
+	alt="Busy"
+/>
+
+<TextContent>
+	<p>The day ended with delicious Italian food and cheesecake.</p>
+</TextContent>
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/poland-2021/20210825-170905328.jpg"
+		width="2200"
+		height="1650"
+		alt="Revel"
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/poland-2021/20210825-174918232.jpg"
+		width="2200"
+		height="1650"
+		alt="Cheesecake"
+	/>
+</Panels>
