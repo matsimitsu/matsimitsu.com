e2e6db31039ee7a4ccc76bbd8e30d1417dc08d4c
Robert Beekman <robert@matsimitsu.nl>
2021-10-07 13:36:10 +0300

    Add day six for Jordan trip

---
+++ src/routes/trips/jordan-2021/day-six.svelte
@@ -0,0 +1,381 @@
+<script context="module">
+	export const postData = {
+		title: 'Day Six',
+		subtitle: 'Wadi Rum',
+		trip: 'jordan-2021',
+		startDate: new Date('2021-10-06'),
+		image: {
+			src: 'https://cdn.matsimitsu.com/jordan-2021/day-six/20211006-02919.jpg',
+			width: 4000,
+			height: 2667,
+			alt: 'Wadi Rum'
+		},
+		locations: [
+			{ name: 'Petra', long: 35.4155337, lat: 30.481418 },
+			{ name: 'Wadi Rum', long: 35.3214502, lat: 29.505919 }
+		]
+	};
+</script>
+
+<script>
+	import TextContent from '$lib/TextContent.svelte';
+	import ResponsiveImage from '$lib/ResponsiveImage.svelte';
+	import Panels from '$lib/Panels.svelte';
+	import TripHeader from '$lib/TripHeader.svelte';
+</script>
+
+<TripHeader {...postData} />
+
+<TextContent>
+	<p>
+		It's another early day today, I had to be in Wadi Rum Village around 09:00, and with a two-hour
+		drive, that meant getting up at 06:30.
+	</p>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/jordan-2021/day-six/20211006-02874.jpg"
+	width="4000"
+	height="2667"
+	alt=""
+/>
+
+<TextContent>
+	<p>
+		Almost exactly on time, I arrived in Wadi Rum village to meet my driver for the day. He'll take
+		me through Wadi Rum in a 4x4.
+	</p>
+	<p>
+		These photos were taken from half-way up a mountain, next to a local spring that emits water
+		throughout the year.
+	</p>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/jordan-2021/day-six/20211006-02877.jpg"
+	width="4000"
+	height="2667"
+	alt=""
+/>
+
+<TextContent>
+	<p>
+		Next up was this sand dune where you can jump, roll, or snowboard off with my previous snowboard
+		attempt fresh in my mind (broken wrist). I opted just to walk up and down. The sand feels
+		amazing on bare feet, if slightly hot.
+	</p>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/jordan-2021/day-six/20211006-02889.jpg"
+	width="4000"
+	height="2667"
+	alt=""
+/>
+
+<TextContent>
+	<p>
+		Opposite the sand dunes were inscriptions into the wall that told early nomads where to go, they
+		were used by caravans bringing food to the bedouins.
+	</p>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/jordan-2021/day-six/20211006-02895.jpg"
+	width="4000"
+	height="2667"
+	alt=""
+/>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/jordan-2021/day-six/20211006-02901.jpg"
+		width="4000"
+		height="2667"
+		alt=""
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/jordan-2021/day-six/20211006-02906.jpg"
+		width="4000"
+		height="2667"
+		alt=""
+	/>
+</Panels>
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/jordan-2021/day-six/20211006-02909.jpg"
+	width="4000"
+	height="2667"
+	alt=""
+/>
+<TextContent
+	><p>
+		As we made our way into the desert, the views became more and more impressive.
+	</p></TextContent
+>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/jordan-2021/day-six/20211006-02919.jpg"
+	width="4000"
+	height="2667"
+	alt=""
+/>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/jordan-2021/day-six/20211006-02966.jpg"
+		width="2667"
+		height="4000"
+		alt=""
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/jordan-2021/day-six/20211006-02953.jpg"
+		width="4000"
+		height="2667"
+		alt=""
+	/>
+</Panels>
+
+<TextContent>
+	<p>
+		Our side-quest of the day was to find the camels of my driver. They are free to roam the
+		country, but there's a place where they are guaranteed to have food and water.
+	</p>
+</TextContent>
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/jordan-2021/day-six/20211006-02932.jpg"
+	width="4000"
+	height="2667"
+	alt=""
+/>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/jordan-2021/day-six/20211006-02987.jpg"
+		width="2667"
+		height="4000"
+		alt=""
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/jordan-2021/day-six/20211006-02983.jpg"
+		width="4000"
+		height="2667"
+		alt=""
+	/>
+</Panels>
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/jordan-2021/day-six/20211006-02941.jpg"
+	width="4000"
+	height="2667"
+	alt=""
+/>
+
+<TextContent>
+	<p>
+		Luckily camels are loud, and with the deafening silence in the reservation, you can hear them
+		from miles away.
+	</p>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/jordan-2021/day-six/20211006-02991.jpg"
+	width="4000"
+	height="2667"
+	alt=""
+/>
+
+<TextContent>
+	<p>
+		After feeding them some snacks and a quick inspection of the ropes, we sent them on their way to
+		the watering place.
+	</p>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/jordan-2021/day-six/20211006-02995.jpg"
+	width="4000"
+	height="2596"
+	alt=""
+/>
+
+<TextContent>
+	<p>
+		The ropes between the front legs are there to prevent the camels from wandering around too much.
+		In the past, they frequently crossed the border with Saudi Arabia, where the police caught and
+		sold them.
+	</p>
+</TextContent>
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/jordan-2021/day-six/20211006-02998.jpg"
+	width="4000"
+	height="2667"
+	alt=""
+/>
+
+<TextContent><p>After a delicious lunch, we went further into the desert.</p></TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/jordan-2021/day-six/20211006-03009.jpg"
+	width="4000"
+	height="2667"
+	alt=""
+/>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/jordan-2021/day-six/20211006-03016.jpg"
+		width="4000"
+		height="2667"
+		alt=""
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/jordan-2021/day-six/20211006-03006.jpg"
+		width="4000"
+		height="2667"
+		alt=""
+	/>
+</Panels>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/jordan-2021/day-six/20211006-03013.jpg"
+	width="4000"
+	height="2667"
+	alt=""
+/>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/jordan-2021/day-six/20211006-03018.jpg"
+		width="4000"
+		height="2667"
+		alt=""
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/jordan-2021/day-six/20211006-03020.jpg"
+		width="4000"
+		height="2667"
+		alt=""
+	/>
+</Panels>
+
+<TextContent>
+	<h2>Mushroom Rock</h2>
+	<p>
+		One of the stopping points was Mushroom Rock, named as such because well.... it looks like one.
+	</p>
+</TextContent>
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/jordan-2021/day-six/20211006-03027.jpg"
+	width="4000"
+	height="2667"
+	alt=""
+/>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/jordan-2021/day-six/20211006-03029.jpg"
+		width="4000"
+		height="2667"
+		alt=""
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/jordan-2021/day-six/20211006-03045.jpg"
+		width="2667"
+		height="4000"
+		alt=""
+	/>
+</Panels>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/jordan-2021/day-six/20211006-03048.jpg"
+	width="3965"
+	height="4000"
+	alt=""
+/>
+
+<TextContent>
+	<p>
+		After a picture of me on an arch, we headed to a canyon where you walk through. (Well, I did, my
+		driver drove around to the other side and took a nap).
+	</p>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/jordan-2021/day-six/20211006-03061.jpg"
+	width="4000"
+	height="2667"
+	alt=""
+/>
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/jordan-2021/day-six/20211006-03095.jpg"
+		width="4000"
+		height="2667"
+		alt=""
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/jordan-2021/day-six/20211006-03123.jpg"
+		width="4000"
+		height="2667"
+		alt=""
+	/>
+</Panels>
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/jordan-2021/day-six/20211006-03137.jpg"
+	width="4000"
+	height="2667"
+	alt=""
+/>
+
+<TextContent>
+	<p>And with that walk, we were nearing sunset, which meant the end of a long day.</p>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/jordan-2021/day-six/20211006-03142.jpg"
+	width="4000"
+	height="2667"
+	alt=""
+/>
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/jordan-2021/day-six/20211006-03176.jpg"
+		width="4000"
+		height="2667"
+		alt=""
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/jordan-2021/day-six/20211006-03194.jpg"
+		width="4000"
+		height="2667"
+		alt=""
+	/>
+</Panels>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/jordan-2021/day-six/20211006-03208.jpg"
+	width="4000"
+	height="2667"
+	alt=""
+/>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/jordan-2021/day-six/20211006-03209.jpg"
+		width="4000"
+		height="2667"
+		alt=""
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/jordan-2021/day-six/20211006-03216.jpg"
+		width="4000"
+		height="2667"
+		alt=""
+	/>
+</Panels>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/jordan-2021/day-six/20211006-03223.jpg"
+	width="4000"
+	height="2667"
+	alt=""
+/>
