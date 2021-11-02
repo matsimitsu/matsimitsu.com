c872e45e0dada0256fa391f2bc7f86191f0819d8
Robert Beekman <robert@matsimitsu.nl>
2021-11-02 13:31:01 +0100

    Add china 2018 day fifteen

---
+++ src/routes/trips/china-2018/day-fifteen.svelte
@@ -0,0 +1,292 @@
+<script context="module">
+	export const postData = {
+		title: 'Day Fifteen',
+		subtitle: 'Tianmen mountain',
+		trip: 'china-2018',
+		startDate: new Date('2018-05-15'),
+		image: {
+			src: 'https://cdn.matsimitsu.com/china-2018/day-fourteen/20180514-06942.jpg',
+			width: 4000,
+			height: 2667,
+			alt: 'Zhangjiajie'
+		},
+		locations: [
+			{
+				src: 'https://cdn.matsimitsu.com/china-2018/day-fifteen/20180515-07098.jpg',
+				width: 4000,
+				height: 2667,
+				alt: 'Tianmen mountain'
+			}
+		]
+	};
+</script>
+
+<script>
+	import TextContent from '$lib/TextContent.svelte';
+	import ResponsiveImage from '$lib/ResponsiveImage.svelte';
+	import Panels from '$lib/Panels.svelte';
+	import TripHeader from '$lib/TripHeader.svelte';
+</script>
+
+<TripHeader {...postData} />
+
+<TextContent>
+	<p>
+		After yesterday's adventure, we stayed a little bit closer to home this time. We took the
+		cablecar that goes right next to our hotel to the top of Tianmen mountain.
+	</p>
+</TextContent>
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/china-2018/day-fifteen/20180515-07024.jpg"
+	width="4000"
+	height="2667"
+	alt="Cable car to Tianmen mountain"
+/>
+
+<TextContent>
+	<p>
+		This cablecar is marketed as "the longest in the world" with a distance of nearly 7.5 kilometers
+		(it takes 30 minutes).
+	</p>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/china-2018/day-fifteen/20180515-07031.jpg"
+	width="4000"
+	height="2667"
+	alt="Cable car to Tianmen mountain"
+/>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/china-2018/day-fifteen/20180515-07033.jpg"
+		width="4000"
+		height="2667"
+		alt="walkway around Tianmen mountain"
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/china-2018/day-fifteen/20180515-07041.jpg"
+		width="4000"
+		height="2667"
+		alt="Tianmen mountain 99-bend road"
+	/>
+</Panels>
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/china-2018/day-fifteen/20180515-07045.jpg"
+	width="4000"
+	height="2667"
+	alt="Tianmen mountain"
+/>
+
+<TextContent>
+	<p>
+		At the top of the mountain, a pathway goes around the edge, giving fantastic views of the
+		surrounding area. Some parts are made of glass.
+	</p>
+</TextContent>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/china-2018/day-fifteen/20180515-07060.jpg"
+		width="4000"
+		height="2667"
+		alt="Tianmen mountain glass walkway"
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/china-2018/day-fifteen/20180515-07063.jpg"
+		width="2667"
+		height="4000"
+		alt="Tianmen mountain view from glass walkway"
+	/>
+</Panels>
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/china-2018/day-fifteen/20180515-07071.jpg"
+	width="4000"
+	height="2667"
+	alt="Tianmen mountain valley view"
+/>
+
+<TextContent>
+	<p>After an ok lunch, we started on the second part of the loop.</p>
+</TextContent>
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/china-2018/day-fifteen/20180515-07079.jpg"
+	width="4000"
+	height="2667"
+	alt="Tianmen mountain temple"
+/>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/china-2018/day-fifteen/20180515-07080.jpg"
+		width="4000"
+		height="2667"
+		alt="Tianmen mountain door"
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/china-2018/day-fifteen/20180515-07082.jpg"
+		width="4000"
+		height="2667"
+		alt="Tianmen mountain statue with locks"
+	/>
+</Panels>
+<TextContent>
+	<p>In the distance, you can see the walkway along the ridge.</p>
+</TextContent>
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/china-2018/day-fifteen/20180515-07084.jpg"
+	width="4000"
+	height="2667"
+	alt="Tianmen mountain"
+/>
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/china-2018/day-fifteen/20180515-07086.jpg"
+	width="4000"
+	height="2667"
+	alt="Tianmen mountain people on a hang bridge with girl making peace sign"
+/>
+
+<ResponsiveImage
+	full
+	src="https://cdn.matsimitsu.com/china-2018/day-fifteen/20180515-07098.jpg"
+	width="4000"
+	height="2667"
+	alt="Tianmen mountain valley"
+/>
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/china-2018/day-fifteen/20180515-07096.jpg"
+		width="2667"
+		height="4000"
+		alt="Tianmen mountain walkways"
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/china-2018/day-fifteen/20180515-07108.jpg"
+		width="4000"
+		height="2667"
+		alt="Tianmen mountain walkways"
+	/>
+</Panels>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/china-2018/day-fifteen/20180515-07118.jpg"
+		width="4000"
+		height="2667"
+		alt="Tianmen mountain red ribbons"
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/china-2018/day-fifteen/20180515-07123.jpg"
+		width="4000"
+		height="2667"
+		alt="Tianmen mountain red ribbons"
+	/>
+</Panels>
+
+<TextContent>
+	<p>
+		We were nearing the end of the loop, and the weather improved a bit. While it never rained there
+		we lots of clouds and the forecast wasn't great (50% chance of rain).
+	</p>
+</TextContent>
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/china-2018/day-fifteen/20180515-07133.jpg"
+	width="4000"
+	height="2667"
+	alt="Tianmen mountain city view"
+/>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/china-2018/day-fifteen/20180515-07132.jpg"
+		width="4000"
+		height="2667"
+		alt="Tianmen mountain temple at the top"
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/china-2018/day-fifteen/20180515-07136.jpg"
+		width="4000"
+		height="2667"
+		alt="Tianmen mountain cable car"
+	/>
+</Panels>
+
+<TextContent>
+	<p>
+		After taking many escalators down, we were nearly at the end of our trip; all we had to do was
+		go down these stairs...
+	</p>
+</TextContent>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/china-2018/day-fifteen/20180515-07140.jpg"
+		width="4000"
+		height="2667"
+		alt="Tianmen mountain escalators"
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/china-2018/day-fifteen/20180515-07160.jpg"
+		width="2667"
+		height="4000"
+		alt="Tianmen mountain stairs"
+	/>
+</Panels>
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/china-2018/day-fifteen/20180515-07151.jpg"
+	width="4000"
+	height="2667"
+	alt="Tianmen mountain stairs"
+/>
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/china-2018/day-fifteen/20180515-07161.jpg"
+		width="4000"
+		height="2667"
+		alt="Tianmen mountain waterfall"
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/china-2018/day-fifteen/20180515-07172.jpg"
+		width="2667"
+		height="4000"
+		alt="Tianmen mountain stairs"
+	/>
+</Panels>
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/china-2018/day-fifteen/20180515-07183.jpg"
+	width="4000"
+	height="2667"
+	alt="Tianmen mountain stairs"
+/>
+<TextContent>
+	<p>...and take a van down this "99 bends" road.</p>
+</TextContent>
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/china-2018/day-fifteen/20180515-07175.jpg"
+	width="4000"
+	height="2667"
+	alt="Tianmen mountain 99 bends"
+/>
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/china-2018/day-fifteen/20180515-07187.jpg"
+		width="4000"
+		height="2667"
+		alt="Tianmen mountain 99 bends"
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/china-2018/day-fifteen/20180515-07189.jpg"
+		width="4000"
+		height="2667"
+		alt="Tianmen mountain 99 bends bus"
+	/>
+</Panels>
+<TextContent>
+	<p>After a short stop at the hotel, we headed out for dinner and called it a day.</p>
+</TextContent>
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/china-2018/day-fifteen/20180515-07199.jpg"
+	width="4000"
+	height="2667"
+	alt="Alley at dusk"
+/>
