0ae4f0c6249f56f43f85727ffeef20a12b32cf04
Robert Beekman <robert@matsimitsu.nl>
2022-05-06 18:39:19 +0800

    Add start of day 3

---
+++ src/routes/trips/asia-2022/day-three.svelte
@@ -0,0 +1,234 @@
+<script context="module">
+	export const postData = {
+		title: 'Day Three',
+		subtitle: 'ArtScience Museum, Singapore',
+		trip: 'asia-2022',
+		startDate: new Date('2022-05-06'),
+		image: {
+			src: 'https://cdn.matsimitsu.com/asia-2022/day-three/20220506-03607.jpg',
+			width: 4000,
+			height: 2667,
+			alt: 'Stairs at Fort Canning Park'
+		},
+		locations: [
+			{
+				name: 'Singapore',
+				long: 103.851959,
+				lat: 1.29027,
+				textPosition: 'left'
+			}
+		]
+	};
+</script>
+
+<script>
+	import TextContent from '$lib/TextContent.svelte';
+	import ResponsiveImage from '$lib/ResponsiveImage.svelte';
+	import Panels from '$lib/Panels.svelte';
+	import TripHeader from '$lib/TripHeader.svelte';
+	import PhonePic from '$lib/PhonePic.svelte';
+</script>
+
+<TripHeader {...postData} />
+
+<TextContent>
+	<p>
+		After walking 20+ Kilometers / 30.000+ steps yesterday, I took it a bit easier today with a
+		visit to the ArtScience Museum, located at the Marina Bay Sands hotel in Singapore.
+	</p>
+	<p>
+		But first, breakfast, this time delicious Indian <i>Murtabak</i>, a pancake filled with chicken
+		and vegetables.
+	</p>
+	<PhonePic subtitle="Foodpic, taken with phone">
+		<ResponsiveImage
+			src="https://cdn.matsimitsu.com/asia-2022/day-three/PXL_20220506_021642979.jpg"
+			width="4032"
+			height="3024"
+			alt="Murtabak"
+		/>
+	</PhonePic>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2022/day-three/20220506-03566.jpg"
+	width="4000"
+	height="2667"
+	alt="Marina Bay Museum, Singapore"
+/>
+
+<TextContent>
+	<p>
+		The exhibit is called "FutureWorld," and it's created by <a
+			href="https://www.teamlab.art/e/artsciencemuseum/"
+			rel="noopener noreferrer"
+			target="_blank">Teamlab</a
+		>, an art collective that created
+		<a href="https://www.matsimitsu.com/trips/japan-2018/day-seven/" rel="preload"
+			>"Borderless" in Tokyo</a
+		>.
+	</p>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2022/day-three/20220506-03543.jpg"
+	width="4000"
+	height="2667"
+	alt="Teamlab, Singapore"
+/>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-three/20220506-03528.jpg"
+		width="2718"
+		height="4000"
+		alt="Teamlab, Singapore"
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-three/20220506-03548.jpg"
+		width="4000"
+		height="2667"
+		alt="Teamlab, Singapore"
+	/>
+</Panels>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2022/day-three/20220506-03531.jpg"
+	width="4000"
+	height="2721"
+	alt="Teamlab, Singapore"
+/>
+
+<TextContent>
+	<p>
+		It's interactive digital art, and with temperatures at 34 degrees which feels like 39, it's nice
+		to be inside for a while.
+	</p>
+</TextContent>
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2022/day-three/20220506-03532.jpg"
+	width="4000"
+	height="2667"
+	alt="Teamlab, Singapore"
+/>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-three/20220506-03533.jpg"
+		width="4000"
+		height="2667"
+		alt="Teamlab, Singapore"
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-three/20220506-03536.jpg"
+		width="4000"
+		height="2667"
+		alt="Teamlab, Singapore"
+	/>
+</Panels>
+<TextContent>
+	<p>The exhibition is a lot smaller than the Tokyo one but still nice to visit.</p>
+</TextContent>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-three/20220506-03549.jpg"
+		width="4000"
+		height="2667"
+		alt="Teamlab, Singapore"
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-three/20220506-03552.jpg"
+		width="2443"
+		height="4000"
+		alt="Teamlab, Singapore"
+	/>
+</Panels>
+
+<ResponsiveImage
+	full
+	src="https://cdn.matsimitsu.com/asia-2022/day-three/20220506-03556.jpg"
+	width="4000"
+	height="2667"
+	alt="Teamlab, Singapore"
+/>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2022/day-three/20220506-03565.jpg"
+	width="4000"
+	height="2667"
+	alt="Marina Bay Museum, Singapore"
+/>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2022/day-three/20220506-03568.jpg"
+	width="4000"
+	height="2667"
+	alt="Singapore skyline"
+/>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-three/20220506-03576.jpg"
+		width="4000"
+		height="2667"
+		alt="Marina Bay Sands"
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-three/20220506-03579.jpg"
+		width="4000"
+		height="2667"
+		alt="Marina Bay Sands"
+	/>
+</Panels>
+
+<ResponsiveImage
+	full
+	src="https://cdn.matsimitsu.com/asia-2022/day-three/20220506-03584.jpg"
+	width="4000"
+	height="2667"
+	alt="Police station"
+/>
+
+<TextContent>
+	<h2>Fort Canning Park</h2>
+	<p>
+		After the museum, I made my way to Fort Canning Park, where I planned to wander around for a
+		while.
+	</p>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2022/day-three/20220506-03591.jpg"
+	width="4000"
+	height="2667"
+	alt="Gate at Fort Canning Park"
+/>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-three/20220506-03586.jpg"
+		width="4000"
+		height="2667"
+		alt="Mural at Fort Canning Park"
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-three/20220506-03590.jpg"
+		width="4000"
+		height="2667"
+		alt="Ruins at Fort Canning Park"
+	/>
+</Panels>
+<TextContent>
+	<p>
+		The sun was out in full force today, and the temperature kept rising, so I cut the visit it
+		short and visited a nearby mall to get Bubble Tea and snacks instead.
+	</p>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2022/day-three/20220506-03607.jpg"
+	width="4000"
+	height="2667"
+	alt="Stairs at Fort Canning Park"
+/>
