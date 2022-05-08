8d7f8c1eff69084242720a5d37c826ab4020ff76
Robert Beekman <robert@matsimitsu.nl>
2022-05-08 19:19:40 +0800

    Add Day Five

---
+++ src/routes/trips/asia-2022/day-five.svelte
@@ -0,0 +1,288 @@
+<script context="module">
+	export const postData = {
+		title: 'Day Five',
+		subtitle: "Rail Corridor and HDB's, Singapore",
+		trip: 'asia-2022',
+		startDate: new Date('2022-05-08'),
+		image: {
+			src: 'https://cdn.matsimitsu.com/asia-2022/day-five/20220508-03826.jpg',
+			width: 4000,
+			height: 2667,
+			alt: 'HDB, Singapore'
+		},
+		locations: [
+			{
+				name: 'Singapore',
+				long: 103.851959,
+				lat: 1.29027,
+				textPosition: 'left'
+			}
+		]
+	};
+</script>
+
+<script>
+	import TextContent from '$lib/TextContent.svelte';
+	import ResponsiveImage from '$lib/ResponsiveImage.svelte';
+	import Panels from '$lib/Panels.svelte';
+	import TripHeader from '$lib/TripHeader.svelte';
+	import PhonePic from '$lib/PhonePic.svelte';
+</script>
+
+<TripHeader {...postData} />
+
+<TextContent>
+	<p>
+		A while ago, a rail corridor running from north to south was converted to a trial, and it's a
+		nice way to visit a few sites near the trail.
+	</p>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-five/map.png"
+		width="1300"
+		height="676"
+		alt="Map of Rail Corridor, Singapore"
+	/>
+</TextContent>
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-five/20220508-03787.jpg"
+		width="2667"
+		height="4000"
+		alt="Singapore Rail Corridor"
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-five/20220508-03788.jpg"
+		width="4000"
+		height="3046"
+		alt="Weird homes, Singapore"
+	/>
+</Panels>
+<TextContent>
+	<p>
+		I picked the middle part to explore a few quarries nearby. These were used to mine granite and
+		now serve as ponds for the local wildlife. You can find bird spotters on most days.
+	</p>
+</TextContent>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-five/20220508-03790.jpg"
+		width="4000"
+		height="2667"
+		alt="Hindhede  Nature Park sign"
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-five/20220508-03791.jpg"
+		width="2667"
+		height="4000"
+		alt="Man walking in Hindhede  Nature Park"
+	/>
+</Panels>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2022/day-five/20220508-03802.jpg"
+	width="4000"
+	height="2667"
+	alt="Singapore Quarry"
+/>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-five/20220508-03792.jpg"
+		width="2667"
+		height="4000"
+		alt="Stairs in Hindhede Nature Park"
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-five/20220508-03795.jpg"
+		width="4000"
+		height="2667"
+		alt="Quarry in Hindhede Nature Park"
+	/>
+</Panels>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2022/day-five/20220508-03807.jpg"
+	width="4000"
+	height="2667"
+	alt="Danger - Keep out sign at Singapore Quarry"
+/>
+
+<TextContent>
+	<p>
+		After visiting two quarries, the Hindhede Quarry and the Singapore Quarry, I took the long way
+		round back home, visiting a few HDB along the way. But not before a little Bao Bun snack.
+	</p>
+	<PhonePic>
+		<Panels>
+			<ResponsiveImage
+				src="https://cdn.matsimitsu.com/asia-2022/day-five/PXL_20220508_060133781.jpg"
+				width="4032"
+				height="3024"
+				alt="Char Siew Bao Bun"
+			/>
+			<ResponsiveImage
+				src="https://cdn.matsimitsu.com/asia-2022/day-five/PXL_20220508_060231236.jpg"
+				width="4032"
+				height="3024"
+				alt="Char Siew Bao Bun"
+			/>
+		</Panels>
+	</PhonePic>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2022/day-five/20220508-03811.jpg"
+	width="4000"
+	height="2937"
+	alt="Automated People Mover in Singapore"
+/>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-five/20220508-03809.jpg"
+		width="2667"
+		height="4000"
+		alt="Singapore HDB"
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-five/20220508-03812.jpg"
+		width="4000"
+		height="2667"
+		alt="Automated People Mover Station in Singapore"
+	/>
+</Panels>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2022/day-five/20220508-03814.jpg"
+	width="4000"
+	height="2958"
+	alt="Singapore smrt"
+/>
+
+<TextContent>
+	<PhonePic subtitle="Egg-tart rule, plus bubble tea">
+		<Panels>
+			<ResponsiveImage
+				src="https://cdn.matsimitsu.com/asia-2022/day-five/PXL_20220508_063614463.jpg"
+				width="4032"
+				height="3024"
+				alt="Portugese Egg-Tart"
+			/>
+			<ResponsiveImage
+				src="https://cdn.matsimitsu.com/asia-2022/day-five/PXL_20220508_083125445.jpg"
+				width="3024"
+				height="4032"
+				alt="Bubble tea"
+			/>
+		</Panels>
+	</PhonePic>
+</TextContent>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-five/20220508-03816.jpg"
+		width="4000"
+		height="2667"
+		alt="SMRT, Singapore"
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-five/20220508-03822.jpg"
+		width="2667"
+		height="4000"
+		alt="Keep Away"
+	/>
+</Panels>
+
+<TextContent>
+	<p>
+		HDB's are Housing and Development Board properties, basically Singapore's public housing
+		projects. While in most countries, public housing means poor people, in Singapore, almost 81
+		percent live in public housing.
+	</p>
+</TextContent>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-five/20220508-03820.jpg"
+		width="2249"
+		height="4000"
+		alt="Stairs HDB, Singapore"
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-five/20220508-03819.jpg"
+		width="4000"
+		height="2667"
+		alt="White HDB, Singapore"
+	/>
+</Panels>
+
+<TextContent>
+	<p>The HDB projects range from modern to a bit older.</p>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2022/day-five/20220508-03825.jpg"
+	width="4000"
+	height="2667"
+	alt="HDB, Singapore"
+/>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2022/day-five/20220508-03828.jpg"
+	width="4000"
+	height="2667"
+	alt="HDB, Singapore"
+/>
+
+<TextContent>
+	<p>
+		On the ground floor and in the courtyards, you can usually find delicious food, while daily life
+		continues above. It's nice to spend an afternoon wandering around the units around the Yishun
+		subway station.
+	</p>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2022/day-five/20220508-03830.jpg"
+	width="4000"
+	height="2453"
+	alt="Man putting out laundry to dry"
+/>
+
+<ResponsiveImage
+	full
+	src="https://cdn.matsimitsu.com/asia-2022/day-five/20220508-03832.jpg"
+	width="4000"
+	height="2667"
+	alt="Singapore HDB"
+/>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2022/day-five/20220508-03833.jpg"
+	width="4000"
+	height="2667"
+	alt="HDB, Singapore"
+/>
+
+<TextContent>
+	<p>
+		Dinner consisted of Bibimbap, from, you guessed it, a food center on the bottom floor of an HDB.
+	</p>
+	<PhonePic subtitle="Bibimbap with Banchan (Side dishes)">
+		<Panels>
+			<ResponsiveImage
+				src="https://cdn.matsimitsu.com/asia-2022/day-five/PXL_20220508_093933377.jpg"
+				width="4032"
+				height="3024"
+				alt="Hankki, Authentic Korean Food"
+			/>
+			<ResponsiveImage
+				src="https://cdn.matsimitsu.com/asia-2022/day-five/PXL_20220508_094503695.jpg"
+				width="4032"
+				height="3024"
+				alt="Bibimbap with Banchan."
+			/>
+		</Panels>
+	</PhonePic>
+</TextContent>
