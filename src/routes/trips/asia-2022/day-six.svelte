b190eda1b1e4a4e5dde31d8b3eb6083050251f20
Robert Beekman <robert@matsimitsu.nl>
2022-05-10 22:22:10 +0800

    Add Day Six

---
+++ src/routes/trips/asia-2022/day-six.svelte
@@ -0,0 +1,253 @@
+<script context="module">
+	export const postData = {
+		title: 'Day Six',
+		subtitle: 'National Museum, Singapore',
+		trip: 'asia-2022',
+		startDate: new Date('2022-05-09'),
+		image: {
+			src: 'https://cdn.matsimitsu.com/asia-2022/day-six/20220509-03868.jpg',
+			width: 4000,
+			height: 2667,
+			alt: 'Singapore National History Museum'
+		},
+		locations: [
+			{
+				name: 'Singapore',
+				long: 103.851959,
+				lat: 1.29027,
+				textPosition: 'left'
+			}
+		]
+	};
+</script>
+
+<script>
+	import TextContent from '$lib/TextContent.svelte';
+	import ResponsiveImage from '$lib/ResponsiveImage.svelte';
+	import Panels from '$lib/Panels.svelte';
+	import TripHeader from '$lib/TripHeader.svelte';
+	import PhonePic from '$lib/PhonePic.svelte';
+</script>
+
+<TripHeader {...postData} />
+
+<TextContent>
+	<p>
+		With the sun at full blast, it was time for an indoor activity, which meant visiting the
+		National Museum of Singapore.
+	</p>
+	<p>But first, breakfast at the local hawker center.</p>
+	<PhonePic subtitle="Chicken rice">
+		<Panels>
+			<ResponsiveImage
+				src="https://cdn.matsimitsu.com/asia-2022/day-six/PXL_20220509_044528929.jpg"
+				width="3024"
+				height="4032"
+				alt=""
+			/>
+			<ResponsiveImage
+				src="https://cdn.matsimitsu.com/asia-2022/day-six/PXL_20220509_045739350.jpg"
+				width="4032"
+				height="3024"
+				alt=""
+			/>
+		</Panels>
+	</PhonePic>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2022/day-six/20220509-03867.jpg"
+	width="4000"
+	height="3161"
+	alt="Singapore National History Museum"
+/>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-six/20220509-03839.jpg"
+		width="2667"
+		height="4000"
+		alt="Singapore National History Museum"
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-six/20220509-03837.jpg"
+		width="4000"
+		height="3039"
+		alt="Singapore National History Museum"
+	/>
+</Panels>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2022/day-six/20220509-03840.jpg"
+	width="4000"
+	height="2667"
+	alt="Singapore National History Museum"
+/>
+
+<TextContent>
+	<p>This museum tells the history of Singapore through a rather fantastic exposition.</p>
+</TextContent>
+
+<ResponsiveImage
+	full
+	src="https://cdn.matsimitsu.com/asia-2022/day-six/20220509-03866.jpg"
+	width="4000"
+	height="2667"
+	alt="National Museum of Singapore"
+/>
+
+<TextContent>
+	<p>
+		It seems TeamLab has also left their mark on this museum with beautiful generated digital art.
+	</p>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2022/day-six/20220509-03846.jpg"
+	width="4000"
+	height="2667"
+	alt="Singapore National History Museum"
+/>
+
+<ResponsiveImage
+	full
+	src="https://cdn.matsimitsu.com/asia-2022/day-six/20220509-03851.jpg"
+	width="4000"
+	height="1554"
+	alt="Teamlab at the Singapore National History Museum"
+/>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2022/day-six/20220509-03849.jpg"
+	width="4000"
+	height="1964"
+	alt=""
+/>
+
+<TextContent>
+	<p>
+		They've put a lot of effort into the displays and built half a ship just to house a couple of
+		photos. It took me a couple of hours to traverse the exposition, and I highly recommend visiting
+		when you're in the neighborhood.
+	</p>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2022/day-six/20220509-03858.jpg"
+	width="4000"
+	height="2667"
+	alt="National Museum of Singapore"
+/>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-six/20220509-03862.jpg"
+		width="2667"
+		height="4000"
+		alt="National Museum of Singapore"
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-six/20220509-03864.jpg"
+		width="4000"
+		height="2667"
+		alt="National Museum of Singapore"
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-six/20220509-03865.jpg"
+		width="4000"
+		height="2667"
+		alt="National Museum of Singapore"
+	/>
+</Panels>
+
+<TextContent>
+	<p>The rest of the day was spent wandering around the area...</p>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2022/day-six/20220509-03870.jpg"
+	width="4000"
+	height="2667"
+	alt="Singapore National History Museum"
+/>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-six/20220509-03876.jpg"
+		width="4000"
+		height="2724"
+		alt=""
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-six/20220509-03874.jpg"
+		width="2667"
+		height="4000"
+		alt=""
+	/>
+</Panels>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-six/20220509-03875.jpg"
+		width="2667"
+		height="4000"
+		alt=""
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-six/20220509-03877.jpg"
+		width="4000"
+		height="2667"
+		alt=""
+	/>
+</Panels>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2022/day-six/20220509-03879.jpg"
+	width="4000"
+	height="2667"
+	alt=""
+/>
+
+<TextContent>
+	<p>...getting dinner in Kampong Glam (the Arab quarter), and a bus ride home.</p>
+	<PhonePic subtitle="Overrice in Kampong Glam">
+		<Panels>
+			<ResponsiveImage
+				src="https://cdn.matsimitsu.com/asia-2022/day-six/PXL_20220509_112234692.jpg"
+				width="4032"
+				height="3024"
+				alt=""
+			/>
+		</Panels>
+	</PhonePic>
+</TextContent>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-six/20220509-03882.jpg"
+		width="4000"
+		height="2667"
+		alt=""
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-six/20220509-03881.jpg"
+		width="2667"
+		height="4000"
+		alt=""
+	/>
+</Panels>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-six/20220509-03885.jpg"
+		width="2667"
+		height="4000"
+		alt=""
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-six/20220509-03883.jpg"
+		width="4000"
+		height="3142"
+		alt=""
+	/>
+</Panels>
