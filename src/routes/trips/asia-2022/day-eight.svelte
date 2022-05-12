12711a68abbf50be8eefd87fb38a37fffdf641fe
Robert Beekman <robert@matsimitsu.nl>
2022-05-12 15:40:07 +0700

    Add day seven and eight

---
+++ src/routes/trips/asia-2022/day-eight.svelte
@@ -0,0 +1,142 @@
+<script context="module">
+	export const postData = {
+		title: 'Day Eight',
+		subtitle: 'Changi airport, Singapore',
+		trip: 'asia-2022',
+		startDate: new Date('2022-05-11'),
+		image: {
+			src: 'https://cdn.matsimitsu.com/asia-2022/day-eight/20220511-04069.jpg',
+			width: 4000,
+			height: 2667,
+			alt: 'Changi Airport Vortex'
+		},
+		locations: [
+			{
+				name: 'Singapore',
+				long: 103.851959,
+				lat: 1.29027,
+				textPosition: 'left'
+			}
+		]
+	};
+</script>
+
+<script>
+	import TextContent from '$lib/TextContent.svelte';
+	import ResponsiveImage from '$lib/ResponsiveImage.svelte';
+	import Panels from '$lib/Panels.svelte';
+	import TripHeader from '$lib/TripHeader.svelte';
+	import PhonePic from '$lib/PhonePic.svelte';
+</script>
+
+<TripHeader {...postData} />
+<TextContent>
+	<p>It's time say goodbye to Singapore, and head to the airport, after coffee of course.</p>
+	<PhonePic subtitle="Coffee and steamed bun">
+		<ResponsiveImage
+			src="https://cdn.matsimitsu.com/asia-2022/day-eight/PXL_20220511_033549062.jpg"
+			width="4032"
+			height="3024"
+			alt=""
+		/>
+	</PhonePic>
+</TextContent>
+
+<ResponsiveImage
+	full
+	src="https://cdn.matsimitsu.com/asia-2022/day-eight/20220511-04053.jpg"
+	width="4000"
+	height="2667"
+	alt="Changi Airport Vortex"
+/>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-eight/20220511-04046.jpg"
+		width="2667"
+		height="4000"
+		alt="Changi Airport Vortex"
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-eight/20220511-04061.jpg"
+		width="4000"
+		height="2667"
+		alt="Changi Airport Vortex"
+	/>
+</Panels>
+
+<TextContent>
+	<p>
+		Changi Airport, its official name, has been rated the best airport several times in a row for
+		the past few years, and their latest addition, called "The Jewel," is one of the many reasons
+		you should arrive hours early for your flight to enjoy the scenery.
+	</p>
+</TextContent>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-eight/20220511-04050.jpg"
+		width="4000"
+		height="2667"
+		alt=""
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-eight/20220511-04065.jpg"
+		width="4000"
+		height="2667"
+		alt=""
+	/>
+</Panels>
+<TextContent>
+	<p>
+		Changi makes this easy by allowing check-in up to 24 hours before a flight instead of the usual
+		3 hours. You can check your bag and explore the airport with only your carry-on backpack.
+	</p>
+</TextContent>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-eight/20220511-04056.jpg"
+		width="4000"
+		height="2667"
+		alt="Changi Airport Vortex"
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-eight/20220511-04090.jpg"
+		width="2385"
+		height="4000"
+		alt="Changi Airport vortex"
+	/>
+</Panels>
+
+<TextContent>
+	<p>There are a million restaurants, and I picked Sateh :)</p>
+	<PhonePic subtitle="Satah">
+		<ResponsiveImage
+			src="https://cdn.matsimitsu.com/asia-2022/day-eight/PXL_20220511_054440676.jpg"
+			width="3024"
+			height="4032"
+			alt="Chicken Sateh"
+		/>
+	</PhonePic>
+</TextContent>
+
+<ResponsiveImage
+	full
+	src="https://cdn.matsimitsu.com/asia-2022/day-eight/20220511-04080.jpg"
+	width="4000"
+	height="2810"
+	alt="Changi Airport Vortex"
+/>
+
+<TextContent>
+	<p>Time to board the flight to Bangkok (BKK)</p>
+	<PhonePic subtitle="Singapore Airlines to BKK">
+		<ResponsiveImage
+			src="https://cdn.matsimitsu.com/asia-2022/day-eight/PXL_20220511_070521188.jpg"
+			width="4032"
+			height="3024"
+			alt="Singapore Airlines to BKK"
+		/>
+	</PhonePic>
+</TextContent>
---
+++ src/routes/trips/asia-2022/day-seven.svelte
@@ -0,0 +1,388 @@
+<script context="module">
+	export const postData = {
+		title: 'Day Seven',
+		subtitle: 'Sentosa, Singapore',
+		trip: 'asia-2022',
+		startDate: new Date('2022-05-10'),
+		image: {
+			src: 'https://cdn.matsimitsu.com/asia-2022/day-seven/20220510-04043.jpg',
+			width: 4000,
+			height: 2667,
+			alt: 'Singapore Skyline with the Marina Bay Sands hotel and the Art Museum.'
+		},
+		locations: [
+			{
+				name: 'Singapore',
+				long: 103.851959,
+				lat: 1.29027,
+				textPosition: 'left'
+			}
+		]
+	};
+</script>
+
+<script>
+	import TextContent from '$lib/TextContent.svelte';
+	import ResponsiveImage from '$lib/ResponsiveImage.svelte';
+	import Panels from '$lib/Panels.svelte';
+	import TripHeader from '$lib/TripHeader.svelte';
+	import PhonePic from '$lib/PhonePic.svelte';
+</script>
+
+<TripHeader {...postData} />
+
+<TextContent>
+	<p>
+		Today, the plan is to visit Sentosa, an island on the south side of Singapore that contains a
+		ton of entertainment, from theme parks such as Universal Studios to Golf courses, Bungee
+		Jumping, Ziplining, beaches, and much more.
+	</p>
+</TextContent>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-seven/20220510-03886.jpg"
+		width="2667"
+		height="4000"
+		alt=""
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-seven/20220510-03887.jpg"
+		width="4000"
+		height="2667"
+		alt=""
+	/>
+</Panels>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-seven/20220510-03891.jpg"
+		width="2667"
+		height="4000"
+		alt=""
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-seven/20220510-03889.jpg"
+		width="3305"
+		height="4000"
+		alt=""
+	/>
+</Panels>
+
+<TextContent>
+	<p>But first breakfast at a local hawker center.</p>
+	<PhonePic subtitle="Chicken over fried rice">
+		<ResponsiveImage
+			src="https://cdn.matsimitsu.com/asia-2022/day-seven/PXL_20220510_064041326.jpg"
+			width="4032"
+			height="3024"
+			alt="Chicken over rice"
+		/>
+	</PhonePic>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2022/day-seven/20220510-03899.jpg"
+	width="4000"
+	height="2667"
+	alt=""
+/>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-seven/20220510-03902.jpg"
+		width="4000"
+		height="2667"
+		alt=""
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-seven/20220510-03896.jpg"
+		width="4000"
+		height="3793"
+		alt=""
+	/>
+</Panels>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2022/day-seven/20220510-03900.jpg"
+	width="4000"
+	height="2667"
+	alt=""
+/>
+
+<TextContent>
+	<p>
+		You can reach Sentosa by either walking, taking the cable car, or the monorail. I picked the
+		monorail since the latter is air-conditioned and the sun is out in full force.
+	</p>
+</TextContent>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-seven/20220510-03904.jpg"
+		width="4000"
+		height="2667"
+		alt="Sentosa Monorail"
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-seven/PXL_20220510_101706982.MP.jpg"
+		width="4032"
+		height="3024"
+		alt="Sentosa Monorail"
+	/>
+</Panels>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2022/day-seven/20220510-03905.jpg"
+	width="4000"
+	height="2667"
+	alt=""
+/>
+
+<TextContent>
+	<p>
+		It's not peak season yet, so most attractions are closed, but I didn't come here for the theme
+		parks. The goal is Fort Siloso, on the very western tip of the Island. You can take the bus
+		there or walk through the "Jungle."
+	</p>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2022/day-seven/20220510-03908.jpg"
+	width="4000"
+	height="2667"
+	alt=""
+/>
+
+<TextContent>
+	<p>
+		"Jungle" in quotation marks because some rocks were suspiciously plastic-y and waterfalls
+		suspiciously artificial.
+	</p>
+</TextContent>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-seven/20220510-03910.jpg"
+		width="4000"
+		height="2667"
+		alt=""
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-seven/20220510-03907.jpg"
+		width="2667"
+		height="4000"
+		alt=""
+	/>
+</Panels>
+
+<TextContent>
+	<p>Look at all this biodiversity.</p>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2022/day-seven/20220510-03911.jpg"
+	width="4000"
+	height="2667"
+	alt=""
+/>
+
+<TextContent>
+	<p>Singapore is an important shipping hub, and there are ships everywhere.</p>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2022/day-seven/20220510-03917.jpg"
+	width="4000"
+	height="2667"
+	alt=""
+/>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2022/day-seven/20220510-03923.jpg"
+	width="4000"
+	height="2667"
+	alt=""
+/>
+
+<TextContent>
+	<p>The fort is the last remaining in Singapore and tells the history of the city's defense.</p>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2022/day-seven/20220510-03925.jpg"
+	width="4000"
+	height="2667"
+	alt="Fort Siloso"
+/>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-seven/20220510-03927.jpg"
+		width="2667"
+		height="4000"
+		alt=""
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-seven/20220510-03928.jpg"
+		width="4000"
+		height="2667"
+		alt=""
+	/>
+</Panels>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-seven/20220510-03932.jpg"
+		width="4000"
+		height="2667"
+		alt=""
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-seven/20220510-03939.jpg"
+		width="2667"
+		height="4000"
+		alt=""
+	/>
+</Panels>
+
+<ResponsiveImage
+	full
+	src="https://cdn.matsimitsu.com/asia-2022/day-seven/20220510-03944.jpg"
+	width="4000"
+	height="2667"
+	alt=""
+/>
+
+<TextContent>
+	<p>
+		There are plenty of scenes from the daily life at the forts, as well as important events, such
+		as the surrender of Japan.
+	</p>
+</TextContent>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-seven/20220510-03946.jpg"
+		width="4000"
+		height="2667"
+		alt=""
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-seven/20220510-03948.jpg"
+		width="2667"
+		height="4000"
+		alt=""
+	/>
+</Panels>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-seven/20220510-03950.jpg"
+		width="4000"
+		height="2667"
+		alt=""
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-seven/20220510-03951.jpg"
+		width="4000"
+		height="3112"
+		alt="Dutch marine"
+	/>
+</Panels>
+
+<TextContent>
+	<p>In the surrounding area, various trails show the local fauna.</p>
+</TextContent>
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2022/day-seven/20220510-03956.jpg"
+	width="4000"
+	height="2667"
+	alt="Sentosa view"
+/>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-seven/20220510-03960.jpg"
+		width="4000"
+		height="2667"
+		alt=""
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/day-seven/20220510-03963.jpg"
+		width="4000"
+		height="2667"
+		alt=""
+	/>
+</Panels>
+
+<TextContent>
+	<p>Time to relax a bit at the beach.</p>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2022/day-seven/20220510-03964.jpg"
+	width="4000"
+	height="2667"
+	alt=""
+/>
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2022/day-seven/20220510-03965.jpg"
+	width="4000"
+	height="2667"
+	alt=""
+/>
+
+<TextContent>
+	<p>
+		It was getting dark; time for dinner and a quick stop-over to the Marina Bay area for a few
+		evening shots of downtown Singapore.
+	</p>
+	<PhonePic subtitle="Wonton noodles with Pork">
+		<ResponsiveImage
+			src="https://cdn.matsimitsu.com/asia-2022/day-seven/PXL_20220510_103506865.jpg"
+			width="4032"
+			height="3024"
+			alt="Wonton noodles with Pork"
+		/>
+	</PhonePic>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2022/day-seven/20220510-03973.jpg"
+	width="4000"
+	height="2667"
+	alt="Singapore skyline"
+/>
+
+<TextContent>
+	<p>The Apple store in Singapore is this huge orb, in front of the Marina Bay Hotel</p>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2022/day-seven/20220510-03967.jpg"
+	width="4000"
+	height="2667"
+	alt="Apple store, Singapore"
+/>
+
+<ResponsiveImage
+	full
+	src="https://cdn.matsimitsu.com/asia-2022/day-seven/20220510-03996.jpg"
+	width="4000"
+	height="2667"
+	alt="Singapore Skyline"
+/>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2022/day-seven/20220510-04004.jpg"
+	width="4000"
+	height="2667"
+	alt="Art Museum, Singapore"
+/>
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2022/day-seven/20220510-04021.jpg"
+	width="4000"
+	height="2667"
+	alt="Marina Bay Sands and Art Museum, Singapore"
+/>
