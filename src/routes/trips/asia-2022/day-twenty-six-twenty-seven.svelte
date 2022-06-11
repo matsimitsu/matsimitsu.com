c89592c7c876411447bc37477a70c4187f48ae33
Robert Beekman <robert@matsimitsu.nl>
2022-06-11 13:23:46 +0700

    Add day 26-27

---
+++ src/routes/trips/asia-2022/day-twenty-six-twenty-seven.svelte
@@ -0,0 +1,257 @@
+<script context="module">
+	export const postData = {
+		title: 'Day Twenty Six - Twenty Seven',
+		subtitle: 'Chiang Mai, Thailand',
+		trip: 'asia-2022',
+		startDate: new Date('2022-05-30'),
+		endDate: new Date('2022-05-31'),
+		image: {
+			src: 'https://cdn.matsimitsu.com/asia-2022/street/20220531-04765.jpg',
+			width: 4000,
+			height: 2667,
+			alt: 'Laundry'
+		},
+		locations: [
+			{
+				name: 'Chiang Mai',
+				long: 98.979263,
+				lat: 18.796143,
+				textPosition: 'left'
+			}
+		]
+	};
+</script>
+
+<script>
+	import TextContent from '$lib/TextContent.svelte';
+	import ResponsiveImage from '$lib/ResponsiveImage.svelte';
+	import Panels from '$lib/Panels.svelte';
+	import TripHeader from '$lib/TripHeader.svelte';
+	import PhonePic from '$lib/PhonePic.svelte';
+	import Location from '$lib/Location.svelte';
+</script>
+
+<TripHeader {...postData} />
+
+<TextContent>
+	<p>
+		This is the week where things are settling down. Patterns are starting to form, and coffee shop
+		personnel remember my order when I walk in.
+	</p>
+	<p>
+		Slowly but surely, things are starting to feel normal and less holiday-y. This is great from a
+		work-focus perspective, but less so from a photography one, as everything looks "normal" now,
+		and instead of taking a photo of something because it looks nice/cool/out of the ordinary from a
+		Dutch perspective, it just looks like it should.
+	</p>
+</TextContent>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/street/20220531-04742.jpg"
+		width="2966"
+		height="2753"
+		alt=""
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/street/20220531-04745.jpg"
+		width="4000"
+		height="2667"
+		alt=""
+	/>
+</Panels>
+
+<TextContent>
+	<h2>Rainy days</h2>
+	<p>
+		It rained this week (it's the rainy season, but so far, it hasn't been raining too much, at
+		least not in the evening when photos look the most amazing). And this means nice reflections
+		everywhere.
+	</p>
+</TextContent>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2022/street/20220531-04747.jpg"
+	width="4000"
+	height="2661"
+	alt=""
+/>
+
+<TextContent>
+	<p>
+		I bought this <a
+			href="https://www.shopmoment.com/products/moment-cinebloom-diffusion-filters/67mm-20-strength"
+			rel="noopener noreferrer"
+			target="_blank">Moment Cinebloom Filter</a
+		>
+		a while ago and ordered the strongest bloomy-ness.
+	</p>
+</TextContent>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/street/20220531-04749.jpg"
+		width="2292"
+		height="4000"
+		alt=""
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/street/20220531-04756.jpg"
+		width="4000"
+		height="2497"
+		alt=""
+	/>
+</Panels>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/street/20220531-04759.jpg"
+		width="4000"
+		height="2667"
+		alt=""
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/street/20220531-04753.jpg"
+		width="2667"
+		height="4000"
+		alt=""
+	/>
+</Panels>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/street/20220531-04761.jpg"
+		width="3650"
+		height="3800"
+		alt=""
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/street/20220531-04763.jpg"
+		width="2398"
+		height="4000"
+		alt=""
+	/>
+</Panels>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/street/20220531-04787.jpg"
+		width="2917"
+		height="3531"
+		alt=""
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/street/20220531-04778.jpg"
+		width="4000"
+		height="2667"
+		alt=""
+	/>
+</Panels>
+<TextContent>
+	<p>
+		In hindsight, I should have bought the 10% bloomy-ness instead of the 20%, as it's a bit much
+		sometimes.
+	</p>
+</TextContent>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/street/20220531-04792.jpg"
+		width="4000"
+		height="2129"
+		alt=""
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/street/20220531-04790.jpg"
+		width="2667"
+		height="4000"
+		alt=""
+	/>
+</Panels>
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/street/20220531-04770.jpg"
+		width="4000"
+		height="2787"
+		alt=""
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/street/20220531-04772.jpg"
+		width="4000"
+		height="2667"
+		alt=""
+	/>
+</Panels>
+<TextContent>
+	<p>It does work out great sometimes, though!</p>
+</TextContent>
+
+<ResponsiveImage
+	full
+	src="https://cdn.matsimitsu.com/asia-2022/street/20220531-04765.jpg"
+	width="4000"
+	height="2667"
+	alt=""
+/>
+
+<ResponsiveImage
+	src="https://cdn.matsimitsu.com/asia-2022/street/20220531-04794.jpg"
+	width="4000"
+	height="3691"
+	alt=""
+/>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/street/20220531-04798.jpg"
+		width="2667"
+		height="4000"
+		alt=""
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/street/20220531-04799.jpg"
+		width="4000"
+		height="2667"
+		alt=""
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/street/20220531-04800.jpg"
+		width="4000"
+		height="2667"
+		alt=""
+	/>
+</Panels>
+
+<TextContent>
+	<p>Maybe it's time to move someplace else to get the "I want to take photos" urge back.</p>
+</TextContent>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/street/20220531-04806.jpg"
+		width="2667"
+		height="4000"
+		alt=""
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/street/20220531-04814.jpg"
+		width="2667"
+		height="4000"
+		alt=""
+	/>
+</Panels>
+
+<Panels>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/street/20220531-04813.jpg"
+		width="4000"
+		height="2667"
+		alt=""
+	/>
+	<ResponsiveImage
+		src="https://cdn.matsimitsu.com/asia-2022/street/20220531-04816.jpg"
+		width="4000"
+		height="2667"
+		alt=""
+	/>
+</Panels>
