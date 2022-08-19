869fb1e5cae9ee4478aa404468e9417db7e95d68
Robert Beekman <robert@matsimitsu.nl>
2022-08-19 20:54:56 +0200

    Fix metadata not appearing for post

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -6,8 +6,28 @@
 	$: ({ post } = data);
 </script>
 
+<svelte:head>
+	<meta name="description" content={post.summary} />
+	<meta property="og:title" content="{post.title} - Matsimitsu.com" />
+	<meta property="og:description" content={post.summary} />
+	<meta property="og:locale" content="en_US" />
+	<meta property="og:type" content="website" />
+	<meta name="twitter:card" content="summary" />
+	<meta name="twitter:creator" content="@matsimitsu" />
+	<meta property="og:url" content="https://matsimitsu.com{post.url}" />
+	<link rel="canonical" href="https://matsimitsu.com{post.url}" />
+	<meta name="twitter:card" content="summary" />
+	<link
+		rel="alternate"
+		type="application/rss+xml"
+		title="Matsimitsu.com - Robert Beekman - Blog RSS Feed"
+		href="/feeds/blog.xml"
+	/>
+</svelte:head>
 <div class="prose prose-lg dark:prose-dark my-24 mx-auto max-w-2xl px-2 2xl:px-0">
 	<h1 class="font-mono text-4xl mb-2 pb-0"># {post.title}</h1>
-	<p class="meta p-0 m-0 font-mono text-base text-gray-400"><FormattedDate date={post.date} /> - {readingTimeInMinutes(post.content)} minutes</p>
+	<p class="meta p-0 m-0 font-mono text-base text-gray-400">
+		<FormattedDate date={post.date} /> - {readingTimeInMinutes(post.content)} minutes
+	</p>
 	<Content content={post.content} />
 </div>
