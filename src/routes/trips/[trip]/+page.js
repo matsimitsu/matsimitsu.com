b5681e00a0aeeab68cdcc659c2397286b01ac0d9
Robert Beekman <robert@matsimitsu.nl>
2022-09-23 22:25:36 +0900

    Reverse posts on trip index page

--- src/routes/trips/[trip]/+page.js
+++ src/routes/trips/[trip]/+page.js
@@ -5,7 +5,7 @@ export async function load({ params, fetch }) {
   const trips = await req.json()
   const trip = trips.find(t => t.url === `/trips/${params.trip}/`)
 
-  const url = `https://pocketbase.matsimitsu.com/api/collections/trip_posts/records?sort=-end&perPage=1000&expand=cover,files,locations&filter=(${encodeURIComponent("trip=\"" + trip.id + "\"&&public=true")})`
+  const url = `https://pocketbase.matsimitsu.com/api/collections/trip_posts/records?sort=+start&perPage=1000&expand=cover,files,locations&filter=(${encodeURIComponent("trip=\"" + trip.id + "\"&&public=true")})`
   const data = await fetch(url)
   const posts = await data.json()
   const enrichedPosts = posts.items.map(({ content = {}, ...post }) => {
